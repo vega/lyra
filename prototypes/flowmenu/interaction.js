bf9d156a88c1e965aa868b43254290342e0f9181
Jeff <jaericson@gmail.com>
2012-11-24 18:45:58 -0800

    Jeff: Implemented Dragging and Scaling for Rect Mark; Dragging for Arc Mark

--- prototypes/flowmenu/interaction.js
+++ prototypes/flowmenu/interaction.js
@@ -1,12 +1,10 @@
-var svgm;
-// stores all active intermediate objects
-var marks=[];
-// intermediate link between the data, d3, and its dropzones
-var markcount=0;
+var svgm; //stores all active intermediate objects
+var marks=[]; //intermediate link between the data, d3, and its dropzones
+var markcount=0; //number of marks on screen
 
 var dataset = [];
 var zonewidth=50;
-//var n;
+var n;
 var allData=[];
 
 var menus = {"rect":
@@ -67,6 +65,7 @@ $(document).ready(function(){
 		}
 		
 		//n=allData.length;
+		allData.push(allData[0]); //push a fake piece of data on the end to serve as the chart selector
 		console.log(allData);
 		
 		//populate list of columns
@@ -76,23 +75,25 @@ $(document).ready(function(){
 			newelement.appendTo($("#data ul"));
 		}
 		
+		//Column class for each variable in the data
 		$(".column").draggable({
 			start:function(event,ui){
 				$(".menudiv").show(); //show available attribute encoders
 			},
 			stop:function(event,ui){
-				$(".menudiv").hide(500); // necessary or drop won't register
+				$(".menudiv").hide(500); //necessary or drop won't register
 			}
 		})
 		.draggable("option","helper","clone");
 	});
 	
 	
-		
+	//Mark boxes at top of screen	
   $(".mark").draggable()
-						.draggable("option", "revert", "invalid")
-						.draggable("option", "helper", "clone");
+						.draggable("option", "revert", "invalid") 
+						.draggable("option", "helper", "clone"); //duplicate of draggable moves with cursor
 
+	//Region is everything below the marks div								
   $("#region").droppable({
 			accept: ".mark",
 			drop: function( event, ui ) {
@@ -107,9 +108,10 @@ $(document).ready(function(){
 					var markID = $(dragged).attr("id").split("_")[1];
 					svgm = d3.select("svg#vis");
 	
-					//dataset=[];
-					//for(var i=0; i<n;i++) dataset.push(20);
-					dataset = new Array(allData.length);
+					dataset=[];
+					n=allData.length;
+					console.log(n);
+					for(var i=0; i<n;i++) dataset.push(1);
 	
 					createMarks(x,y,markcount,markID);
 
@@ -230,7 +232,7 @@ var createMarks=function(x,y,markcount,type) {
 			var rectcont = svgm.append("g")
 			.classed("mark"+markcount,true)
 			.attr("transform", "translate(" + x + "," + y + ")")
-			.attr("fill","steelblue")
+			.attr("fill", "steelblue")
 			.attr("stroke","#ccc")
 			.attr("stroke-width","2");
 	
@@ -242,6 +244,18 @@ var createMarks=function(x,y,markcount,type) {
 			.attr("width",50)
 			.attr("x",0)
 			.attr("y",0)	
+			.attr("fill", function(d,i) {
+				if(i==n-1) { return "#ccc"; }
+				return "steelblue"; })
+			.attr("fill-opacity", function(d,i) {
+				if(i==n-1) { return 0; }
+				return 1; })
+// 			.attr("stroke", function(d,i) {
+// 				if(i==n-1) { return "#000"; }
+// 				return "#ccc"; })
+			.attr("stroke-width", function(d,i) {
+				if(i==n-1) { return 0; }
+				return 2; })
 			.classed("realmark",true)
 			.classed("tempmark",true);
 			break;
@@ -270,6 +284,262 @@ var createMarks=function(x,y,markcount,type) {
 				.classed("realmark",true);
 			break;
 	}
+	
+	mouseX = 0;
+	mouseY = 0;
+	groupX = 0;
+	groupY = 0;
+	groupW = 0;
+	groupH = 0;
+	groupSX = 1;
+	groupSY = 1;
+	isDragging = false;
+	
+	$("g.mark" + markcount).draggable({
+		drag: function(e, ui) {
+			$('body').css('cursor', scaleMode);
+			tSpecs = transformSpecs(e.target);
+			
+			dx = parseInt(ui.position.left - mouseX);
+			dy = parseInt(ui.position.top - mouseY);
+			
+			//console.log(groupX + " + " + ui.position.left + " - " + mouseX + " = " + parseInt(groupX+dx));
+			//console.log("DRAG: " + ui.position.left);
+			
+			switch(scaleMode) {
+				case "move":
+					t = "translate(" + parseInt(groupX+dx) + "," + parseInt(groupY+dy) + ") ";
+					t += "scale(" + tSpecs[2] + "," + tSpecs[3] + ")";
+					$(e.target).attr("transform", t);
+					break;
+					
+				case "e-resize":
+					sx = (groupW+dx)/groupW;
+					t = "translate(" + tSpecs[0] + "," + tSpecs[1] + ") ";
+					t += "scale(" + sx*groupSX + ", " + tSpecs[3] + ")";
+					$(e.target).attr("transform", t);
+					break;
+					
+				case "w-resize":
+					sx = (groupW-dx)/groupW;
+					t = "translate(" + parseInt(groupX+dx) + "," + tSpecs[1] + ") ";
+					t += "scale(" + sx*groupSX + ", " + tSpecs[3] + ")";
+					$(e.target).attr("transform", t);
+					break;
+					
+				case "n-resize":
+					sy = (groupH-dy)/groupH;
+					t = "translate(" + tSpecs[0] + "," + parseInt(groupY+dy) + ") ";
+					t += "scale(" + tSpecs[2] + ", " + sy*groupSY + ")";
+					$(e.target).attr("transform", t);
+					break;
+					
+				case "s-resize":
+					sy = (groupH+dy)/groupH;
+					t = "translate(" + tSpecs[0] + "," + tSpecs[1] + ") ";
+					t += "scale(" + tSpecs[2] + ", " + sy*groupSY + ")";
+					$(e.target).attr("transform", t);
+					break;
+					
+				case "se-resize":
+					sx = (groupW+dx)/groupW;
+					sy = (groupH+dy)/groupH;
+					t = "translate(" + tSpecs[0] + "," + tSpecs[1] + ") ";
+					t += "scale(" + sx*groupSX + ", " + sy*groupSY + ")";
+					$(e.target).attr("transform", t);
+					break;
+					
+				case "ne-resize":
+					sx = (groupW+dx)/groupW;
+					sy = (groupH-dy)/groupH;
+					t = "translate(" + tSpecs[0] + "," + parseInt(groupY+dy) + ") ";
+					t += "scale(" + sx*groupSX + ", " + sy*groupSY + ")";
+					$(e.target).attr("transform", t);
+					break;
+					
+				case "sw-resize":
+					sx = (groupW-dx)/groupW;
+					sy = (groupH+dy)/groupH;
+					t = "translate(" + parseInt(groupX+dx) + "," + tSpecs[1] + ") ";
+					t += "scale(" + sx*groupSX + ", " + sy*groupSY + ")";
+					$(e.target).attr("transform", t);
+					break;
+				
+				case "nw-resize":
+					sx = (groupW-dx)/groupW;
+					sy = (groupH-dy)/groupH;
+					t = "translate(" + parseInt(groupX+dx) + "," + parseInt(groupY+dy) + ") ";
+					t += "scale(" + sx*groupSX + ", " + sy*groupSY + ")";
+					$(e.target).attr("transform", t);
+					break;
+							
+				default:
+					console.log("NADA");
+			}	
+		},
+				
+		
+		start: function(e, ui) {
+			console.log("-START");
+			isDragging = true;
+			
+			tSpecs = transformSpecs(e.target);
+			wh = getDimensions($(e.target).children());
+			
+			mouseX = parseInt(ui.position.left);
+			mouseY = parseInt(ui.position.top);
+
+			groupX = parseInt(tSpecs[0]);
+			groupY = parseInt(tSpecs[1]);
+			
+			groupW = wh[0]*tSpecs[2];
+			groupH = wh[1]*tSpecs[3];
+			
+			groupSX = tSpecs[2];
+			groupSY = tSpecs[3];
+		},
+		
+		
+		stop: function(e, ui) {
+			console.log("-STOP");
+			isDragging = false;
+			
+			//note new width/height
+			tSpecs = transformSpecs(this);
+			wh = getDimensions($(this).children());
+		  
+			console.log("STOP");
+			groupW = wh[0]*tSpecs[2];
+			groupH = wh[1]*tSpecs[3];
+			
+			groupSX = tSpecs[2];
+			groupSY = tSpecs[3];
+    }})
+      
+    
+    .mousedown(function(e) {
+	    //append target to front of the group so it is in the front
+	    //$(e.target).parent().append(e.target);
+	    console.log("-MDOWN");
+	  })
+	  
+	  
+	  .mouseover(function(e) {
+		  console.log("OVER");
+		  if(!isDragging) {
+			  tSpecs = transformSpecs(this);
+				wh = getDimensions($(this).children());
+				groupW = wh[0]*tSpecs[2];
+				groupH = wh[1]*tSpecs[3];
+			}
+	  })
+	  
+	  
+	  .mousemove(function(e) {
+		  tSpecs = transformSpecs(this);
+			
+			//console.log(e.offsetX);
+			//console.log(groupW + " | " + groupH);
+			//console.log(s[1] + "|" + s[2] + "|" + wh[0] + "|" + wh[1] + "|" + e.offsetX + "|" + e.offsetY);
+			
+			if(!isDragging) {
+				getCursorType(tSpecs[0], tSpecs[1], groupW, groupH, e.offsetX, e.offsetY);
+			}
+	  })
+	  
+	  
+	  .mouseout(function(e) {
+		  $('body').css('cursor', 'auto');
+	  });
+
+}
+
+
+var scaleMode = "";
+
+
+
+//Determine cursor type for moving/scaling
+function getCursorType(shapeX, shapeY, shapeW, shapeH, mouseX, mouseY) {
+	pX = (mouseX-shapeX)/shapeW; //percentage of X shape
+	pY = (mouseY-shapeY)/shapeH; //percentage of Y shape
+	
+	if(pX<.1 && pY<.1) {
+		scaleMode = "nw-resize";		
+	} else if(pX<.1 && pY>.9) {
+		scaleMode = "sw-resize";
+	} else if(pX<.1) {
+		scaleMode = "w-resize";
+	} else if(pX>.9 && pY<.1) {
+		scaleMode = "ne-resize";
+	} else if(pX>.9 && pY>.9) {
+		scaleMode = "se-resize";
+	} else if(pX>.9) {
+		scaleMode = "e-resize";
+	} else if(pY<.1) {
+		scaleMode = "n-resize";
+	} else if(pY>.9) {
+		scaleMode = "s-resize";
+	} else {
+		scaleMode = "move";
+	}
+	
+	$('body').css('cursor', scaleMode);	
+}
+
+
+//Get specs from transform attribute of g group
+function transformSpecs(shape) {
+	t = $(shape).attr("transform");
+	p = /\(|\)|\,/g;
+	s = t.split(p);
+	
+	//translateX, translateY, scaleX (default:1), scaleY (default:1)
+	if(s.length<5) {
+		return [s[1], s[2], 1, 1];
+	} else {
+		return [s[1], s[2], s[4], s[5]];
+	}
+}
+
+
+
+//Get the width and height of g group based on its SVG elements (only works for rect now)
+function getDimensions(shapes) {
+	mode = -1; //0 = bar graph horizontal; 1 = bar graph vertical
+	
+	maxW = +$(shapes).eq(0).attr("width");
+	maxH = +$(shapes).eq(0).attr("height");
+	
+	
+	//bar graph with bars of same width or same height?
+	for(i=1; i<shapes.length-1; i++) {
+		if(maxH !== +$(shapes).eq(i).attr("height")) {
+			mode = 0;
+			break;
+		} else if(maxW !== +$(shapes).eq(i).attr("width")) {
+			mode = 1;
+			break;
+		}
+	}
+	
+	if(mode==-1) { return [maxW, maxH]; }
+	
+	maxW = 0, maxH = 0;
+
+	//determine width and height of parent by finding max W/H or cumulative W/H
+	for(i=0; i<shapes.length-1; i++) {
+		if(mode==0) {
+			maxW += +$(shapes).eq(i).attr("width");
+			maxH = Math.max(maxH, +$(shapes).eq(i).attr("height"));
+		}
+		if(mode==1) {
+			maxW = Math.max(maxW, +$(shapes).eq(i).attr("width"));
+			maxH += +$(shapes).eq(i).attr("height");
+		}
+	}
+	
+	return [maxW, maxH];
 }
 
 
@@ -384,18 +654,32 @@ var dropSubMenu=function(event,ui){
 			switch(parameter) {
 				case "height":
 					marks.transition()
-						.attr("height",function(d,i){return yscale(d[colname]+logextra);})
-						.attr("width",20)
-						.attr("x",function(d,i){return i*20;})
-						.attr("y",function(d,i){return 100-yscale(d[colname]+logextra);});
+						.attr("height",function(d,i){
+							if(i==n-1) { return 100; } 
+							return yscale(d[colname]+logextra);})
+						.attr("width",function(d,i) {
+							if(i==n-1) { return 20*(n-1); } 
+							return 20;})
+						.attr("x",function(d,i){
+							if(i==n-1) { return 0; } 
+							return i*20;})
+						.attr("y",function(d,i){
+							if(i==n-1) { return 0; } 
+							return 100-yscale(d[colname]+logextra);});
 					break;
 					
 				case "width":
 					marks.transition()
-						.attr("width",function(d,i){return yscale(d[colname]+logextra);})
-						.attr("height",20)
+						.attr("width",function(d,i){
+							if(i==n-1) { return 100; }
+							return yscale(d[colname]+logextra);})
+						.attr("height", function(d,i) {
+							if(i==n-1) { return 20*(n-1); } 
+							return 20;})
 						.attr("x",function(d,i){return 0;})	
-						.attr("y",function(d,i){return i*20;});
+						.attr("y",function(d,i){
+							if(i==n-1) { return 0; } 
+							return i*20;});
 					break;
 				
 				case "fill":
