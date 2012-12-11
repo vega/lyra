cffa07841205b88795073e9ed7f92427e5cac872
Jeff <jaericson@gmail.com>
2012-12-11 02:01:41 -0800

    Text font input checks if valid system font

--- prototypes/flowmenu/interaction.js
+++ prototypes/flowmenu/interaction.js
@@ -237,6 +237,9 @@ function setPropertyEditorDefaults() {
 	} else {
 		$("tr#innerRadiusRow").hide();
 	}
+	
+	//Third, reset checkmark/x for font found
+	$("span#fontFound").text("");
 		
 
 	//If there is a "note" active
@@ -262,6 +265,7 @@ function setPropertyEditorDefaults() {
 		$("input#updateYPos").val(topPos);
 		
 		$("input#updateTextFont").val(markJQ.css("font-family"));
+		console.log("OH YEAH");
 
 		if(markJQ.css("font-weight")==="bold") {
 			$("button#boldButton").addClass("activeButton");
@@ -458,7 +462,18 @@ $(document).ready(function(){
 	//Property Editor Change: Font
 	$("input#updateTextFont").change(function() {
 		v = $("input#updateTextFont").val();
-		updateFromPropertyEditor(activeMark, "font-family", v);
+		
+		//Check if valid system font
+		d = new Detector();
+		if(d.detect(v)) {
+			updateFromPropertyEditor(activeMark, "font-family", v);
+			$("span#fontFound").html("&#10003;");
+			$("span#fontFound").css("color", "green");
+		} else {
+			$("span#fontFound").html("&#10008;");
+			$("span#fontFound").css("color", "red");
+		}
+		
 	});
 	
 	//Property Editor Change: Bold
@@ -715,6 +730,9 @@ $(document).ready(function(){
 				
 				textbox.draggable();
 				textbox.click(function(){
+					var marknum = getMarkNum($(this));
+					activeMarkOn(marknum);
+					setPropertyEditorDefaults();
 					textbox.draggable("disable");
 					textbox.removeClass("ui-state-disabled"); // removes greying
 				});
@@ -755,6 +773,74 @@ $(document).ready(function(){
 
 
 
+
+/**
+ * JavaScript code to detect available availability of a
+ * particular font in a browser using JavaScript and CSS.
+ *
+ * Author : Lalit Patel
+ * Website: http://www.lalit.org/lab/javascript-css-font-detect/
+ * License: Apache Software License 2.0
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ * Version: 0.15 (21 Sep 2009)
+ *          Changed comparision font to default from sans-default-default,
+ *          as in FF3.0 font of child element didn't fallback
+ *          to parent element if the font is missing.
+ * Version: 0.2 (04 Mar 2012)
+ *          Comparing font against all the 3 generic font families ie,
+ *          'monospace', 'sans-serif' and 'sans'. If it doesn't match all 3
+ *          then that font is 100% not available in the system
+ * Version: 0.3 (24 Mar 2012)
+ *          Replaced sans with serif in the list of baseFonts
+ */
+
+/**
+ * Usage: d = new Detector();
+ *        d.detect('font name');
+ */
+var Detector = function() {
+    // a font will be compared against all the three default fonts.
+    // and if it doesn't match all 3 then that font is not available.
+    var baseFonts = ['monospace', 'sans-serif', 'serif'];
+
+    //we use m or w because these two characters take up the maximum width.
+    // And we use a LLi so that the same matching fonts can get separated
+    var testString = "mmmmmmmmmmlli";
+
+    //we test using 72px font size, we may use any size. I guess larger the better.
+    var testSize = '72px';
+
+    var h = document.getElementsByTagName("body")[0];
+
+    // create a SPAN in the document to get the width of the text we use to test
+    var s = document.createElement("span");
+    s.style.fontSize = testSize;
+    s.innerHTML = testString;
+    var defaultWidth = {};
+    var defaultHeight = {};
+    for (var index in baseFonts) {
+        //get the default width for the three base fonts
+        s.style.fontFamily = baseFonts[index];
+        h.appendChild(s);
+        defaultWidth[baseFonts[index]] = s.offsetWidth; //width for the default font
+        defaultHeight[baseFonts[index]] = s.offsetHeight; //height for the defualt font
+        h.removeChild(s);
+    }
+
+    function detect(font) {
+        var detected = false;
+        for (var index in baseFonts) {
+            s.style.fontFamily = font + ',' + baseFonts[index]; // name of the font along with the base font for fallback.
+            h.appendChild(s);
+            var matched = (s.offsetWidth != defaultWidth[baseFonts[index]] || s.offsetHeight != defaultHeight[baseFonts[index]]);
+            h.removeChild(s);
+            detected = detected || matched;
+        }
+        return detected;
+    }
+
+    this.detect = detect;
+};
 
 
 
@@ -2409,7 +2495,6 @@ var createMarks = function(x, y, markcount, markType) {
 		  activeMarkOn(marknum);		  
 		  setPropertyEditorDefaults();
 	  })
-	  
 	  
 	  .mouseout(function(e) {
 		  $('body').css('cursor', 'auto');
