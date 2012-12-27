9fa323021dd0de786a3a22129a410dfd276e7889
Arvind Satyanarayan <arvindsatya1@gmail.com>
2012-12-27 14:03:55 +0530

    JS to add panels and delegates

--- prototypes/dropzones/js/dropzones.js
+++ prototypes/dropzones/js/dropzones.js
@@ -1,16 +1,71 @@
-var data;
+var fullData;
+var panels = [];
 
 $(document).ready(function(){
         
     $.getJSON("./data/olympics.json", function(response) {
-        data = response;
+        fullData = response;
         // assuming first row is column names
-        for(var label in data[0])
-            $("#ui-data ul").append('<li class="column">' + label + '</li>');
+        for(var label in fullData[0])
+            $("#ui-data ul").append('<li class="data-col">' + label + '</li>');
 
     });
 
     for(var id in primitives)
-        $('#ui-primitives ul').append('<li><img src="imgs/' + id + '.png" /></li>');
+        $('#ui-primitives ul').append('<li class="primitive"><img src="imgs/' + id + '.png" /></li>');
 
 });
+
+function newPanel() {
+    var panel = $('<div></div>')
+        .addClass('vis-panel')
+        .attr('id', 'vis-panel_' + panels.length);
+
+    $('#ui-new-panel').before(panel);
+
+    $('#ui-vis svg').hide();
+    var stage = d3.select('#ui-vis')
+        .append('svg:svg')
+            .attr('class', 'vis-stage')
+            .attr('id', 'vis-stage_' + panels.length);
+
+    panels.push([]);
+}
+
+function addPrimitive(primitive, panelId) {
+    var panel = panels[panelId];
+    var primitiveId = primitive.type + '_' + panelId + '_' + panel.length;
+
+    var delegate = $('<div></div>')
+        .addClass('delegate')
+        .attr('id', primitiveId)
+        .css('top', '15px')
+        .css('left', '75px')
+        .css('width', primitive.delegate.width   + 'px')
+        .css('height', primitive.delegate.height + 'px')
+        .html(primitive.delegate.html);
+
+    for(var dzId in primitive.dropzones) {
+        var dz = primitive.dropzones[dzId];
+
+        var dzNode = $('<div></div>')
+            .addClass('dropzone')
+            .css('left',   dz.x + 'px')
+            .css('top',    dz.y + 'px')
+            .css('width',  dz.width + 'px')
+            .css('height', dz.height + 'px');
+
+        var dzLbl = $('<h3></h3>')
+            .text(dz.label.text)
+            .css('marginLeft', dz.label.x + 'px')
+            .css('marginTop',  dz.label.y + 'px');
+
+        dzNode.append(dzLbl);
+        delegate.append(dzNode);
+    }
+
+    $('#vis-panel_' + panelId).append(delegate);
+
+    primitive.id = primitiveId;
+    panels[panelId].push(primitive);
+}
--- prototypes/dropzones/js/primitives.js
+++ prototypes/dropzones/js/primitives.js
@@ -4,10 +4,8 @@ var primitives = {
 
         delegate: {
             html: '<div class="primitive" style="top: 0px; left: 20px; width: 60px; height: 150px"></div>',
-            height: 175,
-            width: 100,
-            x: 75,
-            y: 15
+            height: 170,
+            width: 100
         },
 
         dropzones: {
@@ -70,10 +68,73 @@ var primitives = {
                     type: 'slider'
                 }]
             }
+        },
+
+        visualization: function(mark, panelId, preview) {
+            var xScale, yScale;
+            var x, y, height, width;
+            var heightMapped = mark.dropzones.height.hasOwnProperty('mapped');
+            var dataCol = heightMapped ? mark.dropzones.height.mapped : mark.dropzones.width.mapped;
+            var data = [];
+            for(var i = 0; i < fullData.length; i++)
+                data.push(fullData[i][dataCol]);
+            var extents = d3.extent(data);
+            var stage = d3.select('svg#vis-stage_' + panelId);
+
+            if(heightMapped) {
+                xScale = d3.scale.linear()
+                    .domain([0, data.length])
+                    .range([0, parseInt(stage.style('width'))]);
+
+                yScale = d3.scale.linear()
+                    .domain(extents)
+                    .range([0, parseInt(stage.style('height'))]);
+
+                width  = 20;
+                height = function(d, i) { return yScale(d); };
+
+                x = function(d, i) { return xScale(i); };
+                y = function(d, i) { 
+                    return parseInt(stage.style('height')) - yScale(d); 
+                };
+
+            } else {
+                xScale = d3.scale.linear()
+                    .domain(extents)
+                    .range([0, parseInt(stage.style('width'))]);
+
+                yScale = d3.scale.linear()
+                    .domain([0, data.length])
+                    .range([0, parseInt(stage.style('height'))]);                    
+
+                width  = function(d, i) { return xScale(d); };
+                height = 15;
+
+                x = 0;
+                y = function(d, i) { return yScale(i); }
+            }
+
+            var marks = stage.selectAll('rect.mark_' + mark.id)
+                .data(data);
+
+            marks.enter()
+                .append('rect')
+                .attr('class', 'mark_' + mark.id)
+
+            marks.transition()
+                .duration(500)
+                .attr('width', width)
+                .attr('height', height)
+                .attr('x', x)
+                .attr('y', y)
+                .attr('fill', 'steelblue')
+                .attr('opacity', function() { 
+                    return preview ? 0.6 : 1; 
+                });
         }
     },
 
     arc: {
-        
+
     }
 };
