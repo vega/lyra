963b7f9dcf3267239ce268b575c47b619472e03a
Arvind Satyanarayan <arvindsatya1@gmail.com>
2014-01-27 16:41:30 -0800

    Timeline service to be able to track states of app for #7 and #17

---
+++ src/js/app/timeline.js
@@ -0,0 +1,34 @@
+vde.App.factory('timeline', ["$rootScope", function($rootScope) {
+  return {
+    timeline: [],
+    currentIdx: 0,
+
+    save: function() {
+      this.timeline[this.currentIdx++] = {
+        vis: vde.Vis.export(),
+        app: {
+          activeVisual: $rootScope.activeVisual.name,
+          isMark: $rootScope.activeVisual instanceof vde.Vis.Mark,
+          activeGroup: $rootScope.activeGroup.name,
+          activePipeline: $rootScope.activePipeline.name
+        }
+      };
+    },
+
+    load: function(idx) {
+      var t = this.timeline[idx], vis = t.vis, app = t.app;
+      vde.Vis.import(vis);
+
+      var g = vde.Vis.groups[app.activeGroup];
+      $rootScope.activeGroup = g;
+      $rootScope.activeVisual = (app.isMark) ?
+          g.marks[app.activeVisual] : g.axes[app.activeVisual];
+
+      $rootScope.activePipeline = vde.Vis.pipelines[app.activePipeline];
+    },
+
+    undo: function() { this.load(--this.currentIdx) },
+    redo: function() { this.load(++this.currentIdx) }
+
+  };
+}]);
