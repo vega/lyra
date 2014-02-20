9ebc08410751e22e32787d9514868dd9ff0e65f0
Arvind Satyanarayan <arvindsatya1@gmail.com>
2014-02-19 22:25:31 -0800

    Use of alpha/beta/gamma can be confusing as it conflicts with existing terminology. Resolves #79

--- src/js/vis/Vis.js
+++ src/js/vis/Vis.js
@@ -273,14 +273,7 @@ vde.Vis = (function() {
   };
 
   vis.codename = function(count) {
-    var letters = ['Alpha', 'Beta', 'Gamma', 'Delta', 'Epsilon', 'Zeta', 'Eta', 'Theta', 'Iota', 'Kappa', 'Lambda', 'Mu', 'Nu', 'Xi', 'Omicron', 'Pi', 'Rho', 'Sigma', 'Tau', 'Upsilon', 'Phi', 'Chi', 'Psi', 'Omega'];
-    var codenames = [];
-    if(count == 0) return letters[0];
-    while(count > 0) {
-      codenames.unshift(letters[count % 10]);
-      count = Math.floor(count / 10);
-    }
-    return codenames.join(" ");
+    return count+1;
   };
 
   return vis;
