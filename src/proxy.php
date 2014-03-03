3f45f83a58a61f547f7d836defb60deeeca01a26
Arvind Satyanarayan <arvindsatya1@gmail.com>
2014-03-03 10:06:23 -0800

    Use CURLOPT_RETURNTRANSFER

--- src/proxy.php
+++ src/proxy.php
@@ -3,7 +3,11 @@
 $headers = getallheaders();
 if(strpos($headers['Referer'], 'http://idl.cs.washington.edu/projects/lyra/') !== false) {
   $curl = curl_init($_GET['url']);
-  echo curl_exec($curl);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+  $result = curl_exec($curl);
+  curl_close ($curl);
+  echo $result;
 } else {
   echo '[{ "error": "Error proxying script, did not originate from a Lyra URL"}]';
 }
