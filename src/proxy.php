899ea310f5d05a51b9d7064eb4634e593f07f7bd
Arvind Satyanarayan <arvindsatya1@gmail.com>
2014-03-04 12:04:22 -0800

    Parse URL to check http(s)

--- src/proxy.php
+++ src/proxy.php
@@ -1,15 +1,23 @@
-<?php 
+<?php
 
 $headers = getallheaders();
-if(strpos($headers['Referer'], 'http://idl.cs.washington.edu/projects/lyra/') !== false) {
-  $curl = curl_init($_GET['url']);
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+$url = $_GET['url'];
 
-  $result = curl_exec($curl);
-  curl_close ($curl);
-  echo $result;
-} else {
-  echo '[{ "error": "Error proxying script, did not originate from a Lyra URL"}]';
+if(strpos($headers['Referer'], 'http://idl.cs.washington.edu/projects/lyra/') === false) {
+  echo '[{ "error": "Error proxying script, did not originate from a Lyra URL" }]';
+  return;
 }
+
+if(strpos(parse_url($url)['scheme'], 'http') === false) {
+  echo '[{ "error": "We only support data found at a http(s) URL." }]';
+  return;
+}
+
+$curl = curl_init($_GET['url']);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+$result = curl_exec($curl);
+curl_close ($curl);
+echo $result;
 
 ?>
