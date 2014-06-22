6ff615bc16b978052f691172875987ca3d12bb67
Ryan Russell <russellsprouts2@gmail.com>
2014-06-21 18:40:57 -0700

    Simplified file includes in Karma test runner

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,32 +15,24 @@ module.exports = function(grunt) {
       unit: {
         options: {
           frameworks: ['jasmine'],
+          //All of the files that the app needs. The order is important --
+          //many files depend on others.
           files: [
             "src/js/lib/jquery.min-1.9.0.js",
-            "src/js/lib/jquery-migrate-1.2.1.js",
-            "src/js/lib/jquery.event.drag-2.2.js",
-            "src/js/lib/jquery.event.drop-2.2.js",
-            "src/js/lib/jquery-ui.min-1.9.0.js",
+            "src/js/lib/**/jquery*.js",
             "src/js/lib/bootstrap.min.js",
-            "src/js/lib/jquery.dataTables-1.9.4.js",
+            "src/js/lib/bootstrap-colorpicker-module.js",
             "src/js/lib/datatables/FixedColumns.js",
             "src/js/lib/d3.v3.min.js",
             "src/js/lib/vega-1.3.3.js",
             "src/js/lib/angular-1.2.10.min.js",
             "src/js/lib/angular-1.2.10-mocks.js",
-            "src/js/lib/bootstrap-colorpicker-module.js",
             "src/js/lib/inflector.js",
             "src/js/lib/sortable.js",
             "src/js/lib/indexeddb.js",
 
             "src/js/app/vde.js",
-            "src/js/app/timeline.js",
-            "src/js/app/draggable.js",
-            "src/js/app/groups.js",
-            "src/js/app/inspectors.js",
-            "src/js/app/pipelines.js",
-            "src/js/app/data.js",
-            "src/js/app/export.js",
+            "src/js/app/**/*.js",
 
             "src/js/vis/Vis.js",
             "src/js/vis/iVis.js",
@@ -58,16 +50,7 @@ module.exports = function(grunt) {
             "src/js/vis/Scale.js",
             "src/js/vis/Axis.js",
             "src/js/vis/Transform.js",
-            "src/js/vis/transforms/Sort.js",
-            "src/js/vis/transforms/Filter.js",
-            "src/js/vis/transforms/Formula.js",
-            "src/js/vis/transforms/Pie.js",
-            "src/js/vis/transforms/Facet.js",
-            "src/js/vis/transforms/Stack.js",
-            "src/js/vis/transforms/Geo.js",
-            "src/js/vis/transforms/Force.js",
-            "src/js/vis/transforms/Stats.js",
-            "src/js/vis/transforms/Window.js",
+            "src/js/vis/transforms/**/*.js",
 
             "tests/unit/**/*.js"
           ]
