05433fdb0106f21638f8b12cbe54885775c35a46
Ryan Russell <russellsprouts2@gmail.com>
2014-06-23 17:11:34 -0700

    update protractor to latest version

--- tests.js
+++ tests.js
@@ -2,8 +2,7 @@
 exports.config = {
   // Do not start a Selenium Standalone sever - only run this using chrome.
   chromeOnly: true,
-  chromeDriver: './node_modules/protractor/selenium/chromedriver',
-
+  
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
     'browserName': 'chrome'
