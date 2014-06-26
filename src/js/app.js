39cba3a3f822a921b95a03ccf9d9e68c5bbef1f5
Arvind Satyanarayan <arvindsatya1@gmail.com>
2014-06-25 22:33:17 -0700

    vde-field fix with aggregation

--- src/js/app.js
+++ src/js/app.js
@@ -936,8 +936,12 @@ vde.App.directive('vdeField', function(Vis) {
   return {
     scope: { vdeField: '=' },
     link: function(scope, element, attrs) {
-      scope.$watch('vdeField', function() {
-        if(scope.vdeField instanceof Vis.Field) 
+      var isField = scope.vdeField instanceof Vis.Field;
+
+      scope.$watch(function(scope) { 
+        return isField ? scope.vdeField.spec() : false;
+      }, function() {
+        if(isField)
           element.data('field', scope.vdeField)
             .attr('field-spec', scope.vdeField.spec());
       });
