96e640e24e49b00884f36edb10168ec0e51b00d6
Ryan Russell <russellsprouts2@gmail.com>
2014-07-01 14:38:05 -0700

    Added more controller tests

---
+++ tests/unit/TimelineCtrl.spec.js
@@ -0,0 +1,38 @@
+describe("TimelineCtrl", function() {
+	var timeout, scope, rootScope, window, timeline, ctrl;
+	beforeEach(module("vde"));
+
+	beforeEach(inject(function($rootScope, $timeout, $controller, $q) {
+		timeout = $timeout;
+		rootScope = $rootScope.$new();
+		scope = rootScope.$new();
+
+		window = jasmine.createSpyObj('window', ['addEventListener']);
+		timeline = jasmine.createSpyObj('timeline', ['undo', 'redo', 'files']);
+		timeline.files.and.returnValue({
+			getAll:function() {
+				return $q.when([{fileName:'file'}]);
+			}
+		});
+
+		ctrl = $controller('TimelineCtrl', {
+			$scope: scope,
+			$rootScope: rootScope,
+			$window: window,
+			timeline: timeline,
+			$timeout: timeout
+		});
+	}));
+
+	describe('Undo/Redo', function() {
+		it('should undo', function() {
+			scope.undo();
+			expect(timeline.undo).toHaveBeenCalled();
+		});
+
+		it('should redo', function() {
+			scope.redo();
+			expect(timeline.redo).toHaveBeenCalled();
+		});
+	});
+});
--- tests/unit/timeline.spec.js
+++ tests/unit/timeline.spec.js
@@ -1,13 +1,16 @@
 describe('timeline', function() {
-	var rootScope, db, Vis, iVis, timeline;
+	var rootScope, db, Vis, iVis, timeline, objectStore;
   beforeEach(function() {
     module('vde');
   });
 
   beforeEach(function() {
     rootScope = jasmine.createSpyObj('$rootScope', ['$apply']);
+    rootScope.$root = {$$phase: true};
     db = jasmine.createSpyObj('$indexedDB', ['objectStore']);
-    db.objectStore.and.returnValue(['list','of','files']);
+    objectStore = jasmine.createSpyObj('objectStore', ['find']);
+    db.objectStore.and.returnValue(objectStore);
+
     Vis = jasmine.createSpyObj('Vis', ['export','import']);
     iVis = jasmine.createSpyObj('iVis', ['show']);
     module(function($provide) {
@@ -18,41 +21,43 @@ describe('timeline', function() {
     });
   });
 
-  beforeEach(inject(function(_timeline_) {
+  beforeEach(inject(function(_timeline_, $q) {
     timeline = _timeline_;
+    Vis.import.and.returnValue({then:function(f) {
+      return f({imported: true});
+    }});
+    objectStore.find.and.returnValue({});
   }));
 
-  describe('files', function() {
-    it('should give list of files', function() {
-      expect(timeline.files()).toEqual(['list','of','files']);
-    });
-  });
+  beforeEach(function() {
+    var i;
+    Vis.export.and.callFake(function(){ return {VisVersion: i}; });
+    Vis.Mark = function() {};
+    Vis.marks = {Group:function() {}};
+    for(i = 0; i < 25; i++) {
+      rootScope.activeVisual = {name: "Visual " + i};
+      rootScope.activeLayer = {name: "Layer " + i};
+      rootScope.activeGroup = {name: "Group " + i};
+      rootScope.activePipeline = {name: "Pipeline " + i};
 
-  describe('open', function() {
-
-  });
-
-  describe('store', function() {
-
-  });
-
-  describe('delete', function() {
-
-  });
-
-  describe('save', function() {
-
-  });
-
-  describe('load', function() {
-
+      timeline.save();
+    }
   });
 
   describe('undo', function() {
+    it('should ', function() {
+      var time = timeline.timeline;
+      expect(time[time.length-1].vis.VisVersion).toBe(24);
 
+      timeline.undo();
+
+      expect(rootScope.activeVisual.name).toBe("Visual 23");
+    });
   });
 
   describe('redo', function() {
+    it('should', function() {
 
+    });
   });
 });
