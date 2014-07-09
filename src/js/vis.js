a90fbf95ff4deb9d0a836f0aa3f6ec612a8939f8
Ryan Russell <russellsprouts2@gmail.com>
2014-07-09 11:54:16 -0700

    Units tests for Transform

--- src/js/vis.js
+++ src/js/vis.js
@@ -2057,7 +2057,7 @@ vde.Vis.callback = (function() {
 })();
 vde.Vis.Field = (function() {
   var field = function(name, accessor, type, pipelineName, stat) {
-    if(arguments[0] instanceof vde.Vis.Field) {
+    if(typeof arguments[0] === 'object') {
       var f = arguments[0]; name = f.name;
       accessor = f.accessor; type = f.type;
       pipelineName = f.pipelineName; stat = f.stat;
