1afcf844f382cae98755df67df12de774ee6e16f
Arvind Satyanarayan <arvindsatya1@gmail.com>
2015-12-21 18:33:14 -0800

    Testing production rules with symbol marks.

--- test/_start.js
+++ test/_start.js
@@ -26,6 +26,16 @@ global.image = function(name) {
 };
 global.image.save = false;
 
+// Load cars dataset for testing
+
+before(function() {
+  var cars = global.cars = model.pipeline('cars');
+  cars._source.init({ 
+    values: JSON.parse(fs.readFileSync(res + 'cars.json')) 
+  });
+  cars.f = function(name) { return cars._source.schema()[name]._id; };
+});
+
 // Start each test with a fresh model/vis.
 beforeEach(function() {
   model = model.init();
