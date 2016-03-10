b8eb17e2ca39cf25cda36ac71c2ebf288d5a3fc4
K. Adam White <kadamwhite@users.noreply.github.com>
2016-03-10 17:57:35 -0500

    Include source files even if not under test for accurate coverage
    Code coverage can only be determined for files that are loaded into
    the test runner bundle: nothing else is loaded by Karma, and therefore
    nothing else can be instrumented by Karma.
    
    This trick expands on the "alternative" karma-webpack loading model to
    load in all non-entrypoint application source files, so that they will
    be picked up by Istanbul when generating coverage reports.
    
    (Including index.js files causes several errors, so it is easiest to
    exclude them wholesale for now. Down the road some components may benefit
    from a test spec for the index or aggregator itself.)

--- test_index.js
+++ test_index.js
@@ -2,9 +2,30 @@
 // "alternative usage" https://github.com/webpack/karma-webpack#alternative-usage and
 // https://webpack.github.io/docs/context.html for more, ahem, "context".
 
-// Require all JS and JSX test spec files
-var context = require.context('./src/js', true, /\.test\.jsx?$/);
+var indexFileRE = /index.js/;
+var testFileRE = /\.test\.js/;
 
-// `require.context` returns a customized require function: invoke that function
-// to require each matched test spec file
-context.keys().forEach(context);
+// Build a list of all JS and JSX files in the application source directory
+var context = require.context('./src/js', true, /\.jsx?$/);
+
+// `require.context` returns a customized require function: we will invoke that
+// function for each file path we actually want to include in our bundle.
+
+// Files must be included in our test bundle in order to be instrumented for code
+// coverage reporting. To this end, we start by filtering the file list down to
+// only non-spec, non-index files (index files are excluded because they are
+// generally entry points that cause side-effects).
+//
+// It's important to note that the tests will run if we skip this step: we'd
+// simply only have coverage reports for files that are actually required from
+// our test specs. See https://github.com/deepsweet/istanbul-instrumenter-loader
+// for more information.
+context.keys().filter(function( path ) {
+  return ! ( indexFileRE.test( path ) || testFileRE.test( path ) );
+}).forEach(context);
+
+// Finally, extract the test spec files from that list and require them to kick
+// off our unit tests.
+context.keys().filter(function( path ) {
+  return testFileRE.test( path );
+}).forEach(context);
