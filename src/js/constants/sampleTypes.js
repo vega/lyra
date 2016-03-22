a6db68b52e0353db833f67565828e4f70fd5b464
K. Adam White <kadamwhite@users.noreply.github.com>
2016-03-22 11:34:47 -0400

    Remove sample reducers and actions

--- src/js/constants/sampleTypes.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-module.exports.ADD_TODO = 'ADD_TODO';
-module.exports.DELETE_TODO = 'DELETE_TODO';
-module.exports.EDIT_TODO = 'EDIT_TODO';
-module.exports.MARK_TODO = 'MARK_TODO';
-module.exports.MARK_ALL = 'MARK_ALL';
-module.exports.CLEAR_MARKED = 'CLEAR_MARKED';
