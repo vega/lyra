8a6851524d0a74be39da778b85f42a5392f6bc84
K. Adam White <kadamwhite@users.noreply.github.com>
2016-03-30 11:42:59 -0400

    Do not parse SCSS in Karma & omit js/index.js in Karma (fixes #314)
    raw-loader includes the files, but includes them as raw data. The extra
    file size overhead that entails isn't relevant within a testing environment,
    and it gets the SCSS out of the way without impacting the JS around it.
    
    IgnorePlugin prevents that file from being evaluated, which doesn't have any
    effect on the CSS warning (that's taken care of by the raw-loader), but rather
    fixes an issue where the root index.js file was being parsed and possibly causing
    side-effects.
    
    Props @mzgoddard, who helped identify that `require.context` did cause Webpack
    to parse & traverse a file tree even if that file was not run through the
    resulting context function. (Writing a more complex regex to omit the top-level
    index file from the `require.context()` call was considered, but then abandoned
    in favor of the more obvious plugin solution)

--- test_index.js
+++ test_index.js
@@ -2,7 +2,7 @@
 // "alternative usage" https://github.com/webpack/karma-webpack#alternative-usage and
 // https://webpack.github.io/docs/context.html for more, ahem, "context".
 
-var indexFileRE = /index.js/;
+var appInitializerRE = /(components|\.)\/index.js/;
 var testFileRE = /\.test\.js/;
 
 // Build a list of all JS and JSX files in the application source directory
@@ -11,21 +11,31 @@ var context = require.context('./src/js', true, /\.jsx?$/);
 // `require.context` returns a customized require function: we will invoke that
 // function for each file path we actually want to include in our bundle.
 
-// Files must be included in our test bundle in order to be instrumented for code
-// coverage reporting. To this end, we start by filtering the file list down to
-// only non-spec, non-index files (index files are excluded because they are
-// generally entry points that cause side-effects).
+// Files must be included in our test bundle in order to be instrumented for
+// code coverage reporting. To this end, we start by loading all non-test files
+// to ensure we get reports for files that are not yet tested, so that "100%"
+// coverage really will be.
+//
+// The two exceptions are the root index.js file (which is also suppressed via
+// IgnorePlugin, see "note also" below) and the root component file which
+// renders the root React component into the document; both of these are for
+// kicking off the app in a live environment, and cause undesireable side-
+// effects when run in a test environment.
 //
 // It's important to note that the tests will run if we skip this step: we'd
 // simply only have coverage reports for files that are actually required from
 // our test specs. See https://github.com/deepsweet/istanbul-instrumenter-loader
 // for more information.
-context.keys().filter(function( path ) {
-  return ! ( indexFileRE.test( path ) || testFileRE.test( path ) );
+//
+// Note also that Webpack will _parse_ the files even if they are filtered out
+// here, so if you want to exclude a file's existence from Webpack entirely then
+// an IgnorePlugin in Karma's Webpack configuration is a more thorough solution.
+context.keys().filter(function(path) {
+  return !testFileRE.test(path) && !appInitializerRE.test(path);
 }).forEach(context);
 
 // Finally, extract the test spec files from that list and require them to kick
 // off our unit tests.
-context.keys().filter(function( path ) {
-  return testFileRE.test( path );
+context.keys().filter(function(path) {
+  return testFileRE.test(path);
 }).forEach(context);
