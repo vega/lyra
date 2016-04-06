b1725e2486ab7999b9b0e633df1d347e82bb424f
K. Adam White <kadamwhite@users.noreply.github.com>
2016-04-06 18:41:07 -0400

    Fix counter tests

--- src/js/util/counter.test.js
+++ src/js/util/counter.test.js
@@ -66,14 +66,48 @@ describe('counter utility', function() {
       for (i = 0; i < 7; i++) {
         scaleResults.push(counter.type('scale'));
       }
-      expect(groupResults).to.deep.equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-      expect(counter.type('group')).to.equal(10);
+      expect(groupResults).to.deep.equal([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+      expect(counter.type('group')).to.equal(11);
 
       expect(rectResults).to.deep.equal([1, 2, 3, 4, 5]);
       expect(counter.type('rect')).to.equal(6);
 
       expect(scaleResults).to.deep.equal([1, 2, 3, 4, 5, 6, 7]);
       expect(counter.type('scale')).to.equal(8);
+    });
+
+  });
+
+  describe('reset() method', function() {
+
+    it('is a function', function() {
+      expect(counter).to.have.property('reset');
+      expect(counter.reset).to.be.a('function');
+    });
+
+    it('resets the global counter', function() {
+      expect(counter.global()).to.equal(1);
+      expect(counter.global()).to.equal(2);
+      counter.reset();
+      expect(counter.global()).to.equal(1);
+      expect(counter.global()).to.equal(2);
+      counter.reset();
+      expect(counter.global()).to.equal(1);
+    });
+
+    it('resets type counters', function() {
+      expect(counter.type('group')).to.equal(1);
+      expect(counter.type('group')).to.equal(2);
+      expect(counter.type('rect')).to.equal(1);
+      expect(counter.type('rect')).to.equal(2);
+      counter.reset();
+      expect(counter.type('group')).to.equal(1);
+      expect(counter.type('group')).to.equal(2);
+      expect(counter.type('rect')).to.equal(1);
+      expect(counter.type('rect')).to.equal(2);
+      counter.reset();
+      expect(counter.type('group')).to.equal(1);
+      expect(counter.type('rect')).to.equal(1);
     });
 
   });
