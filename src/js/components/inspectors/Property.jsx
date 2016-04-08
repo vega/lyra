61d84cbf054e423565c7ce7243b47c90e89b1b22
Arvind Satyanarayan <arvindsatya1@gmail.com>
2016-04-08 16:23:25 -0700

    Unbind by clicking bound property's scale/field. Strikethrough on hover.

--- src/js/components/inspectors/Property.jsx
+++ src/js/components/inspectors/Property.jsx
@@ -74,10 +74,10 @@ var Property = React.createClass({
 
     labelEl = labelEl || (<label htmlFor={name}>{label}</label>);
     scaleEl = scale && (scale = lookup(scale)) ?
-      (<div className="scale">{scale.name}</div>) : null;
+      (<div className="scale" onClick={this.unbind}>{scale.name}</div>) : null;
 
     controlEl = field && (field = lookup(field)) ?
-      (<div className="field">{field._name}</div>) : controlEl;
+      (<div className="field" onClick={this.unbind}>{field._name}</div>) : controlEl;
 
     if (!controlEl) {
       switch (type) {
