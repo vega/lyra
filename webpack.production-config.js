9698c706f3c7f7fdc1b80ccc6e32cdd6034d3e11
K. Adam White <kadamwhite@users.noreply.github.com>
2016-04-28 17:44:19 -0400

    Add a production-mode build to Lyra
    This adds a `npm run build:prod` script, which uses a new production-mode
    wrapper for the webpack config to set certain optimization options useful
    when deploying Lyra as a standalone app on a static file server.
    
    The key changes in a production-mode build:
    
    1. Use of the [`-p` Webpack config option](https://webpack.github.io/docs/cli.html#production-shortcut-p) to enable Webpack's build optimizations
    2. Use of a different sourcemap option
    3. Use of a DefinePlugin to globally hard-set a "production mode" constant to permit Redux and other code to opt-in to performance-tuned builds instead of development-tool builds
    
    With this change, a complete "deployment" workflow will look like this:
    
    1. `npm run build:prod` to trigger a build in production mode
    2. Copy `index.html`, `build/`, and `data/` to a server
    3. Lyra should now be usable on that server
    
    Partially addresses #362 by providing tools for generating a truly standalone
    build: webpack-dev-server was never intended for production use.

---
+++ webpack.production-config.js
@@ -0,0 +1,21 @@
+// Set global environment to production, to trigger the use of an optimized
+// Redux build
+// process.env.NODE_ENV = 'production';
+
+var webpack = require('webpack');
+
+var devConfig = require('./webpack.config');
+
+var productionConfig = Object.assign({}, devConfig, {
+  devtool: 'cheap-module-source-map'
+});
+
+// Add a DefinePlugin instance to hard-set global environment to "production",
+// to trigger the use of an optimized Redux build, etc
+productionConfig.plugins.push(new webpack.DefinePlugin({
+  'process.env': {
+    NODE_ENV: JSON.stringify('production')
+  }
+}));
+
+module.exports = productionConfig;
