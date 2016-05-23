f2ca543898db71191aceb85d529c1fcef7b5ad2b
Nikhil Khanna <nikhilkhanna@Nikhils-MacBook-Pro.local>
2016-05-23 12:56:21 -0700

    remove log

--- src/js/model/rules/data.js
+++ src/js/model/rules/data.js
@@ -15,6 +15,7 @@ var getParent = require('../../util/hierarchy').getParent;
  * @returns {void}
  */
 function data(parsed, from) {
+  // TODO: multiple datasets per pipeline
   var map = this._rule._map.data;
   // This is a super-simple map to map b/w names in the vega output
   // VL's data[0].name.source maps to something in lyra e.g.
