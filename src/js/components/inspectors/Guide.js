0d2b5e21ee2ab3e999fe0fa51f098a11a9555c09
Anjir Hossain <anjirhossain@gmail.com>
2016-06-29 10:04:18 -0700

    Guide delete/add and GuideInspector [WIP]

---
+++ src/js/components/inspectors/Guide.js
@@ -0,0 +1,13 @@
+'use strict';
+var React = require('react'),
+    Property = require('./Property');
+
+var GuideInspector = React.createClass({
+    render: function() {
+        var props = this.props;
+
+        return (
+            
+        );
+    }
+});
