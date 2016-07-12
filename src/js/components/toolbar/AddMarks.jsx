vega/lyra

commit ec40dcbb3d305b857d7bbc8afcf453d0515cf6cf

Removing model.lookup calls from React components.

src/js/util/hierarchy.js
@@ -1,33 +1,37 @@
 'use strict';
 
+var store = require('../store'),
+    state = store.getState(),
+    getIn = require('./immutable-utils').getIn;
+
 /**
  * Find the parent item for a given mark.
  *
- * @param {Primitive} mark - A mark for which to return the parent mark
- * @returns {Primitive|null} The requested mark, if present, else null
+ * @param {ImmutableMap} mark - A mark for which to return the parent mark
+ * @returns {ImmutableMap|null} The requested mark, if present, else null
  */
 function getParent(mark) {
   // Require model in here to sidestep circular dependency issue
-  return require('../model').lookup(mark._parent) || null;
+  return getIn(state, 'marks.' + mark.get('_parent')) || null;
 }
 
 /**
  * Get all parent nodes for a given primitive in the Lyra hierarchy, i.e. all
  * groups which may be considered to be ancestors of the provided primitive.
  *
- * @param  {Primitive} primitive - The primitive for which to return ancestors.
- * @returns {Primitive[]} An array of primitives.
+ * @param  {ImmutableMap} primitive - The primitive for which to return ancestors.
+ * @returns {ImmutableMap[]} An array of primitives.
  */
 function getParents(primitive) {
   if (!primitive) {
     return [];
   }
-  var current = primitive._parent && getParent(primitive);
+  var current = primitive.get('_parent') && getParent(primitive);
   if (!current) {
     return [];
   }
   var parents = [current];
-  while (current && current._parent) {
+  while (current && current.get('_parent')) {
     current = getParent(current);
     if (current) {
       parents.push(current);
@@ -39,13 +43,13 @@ function getParents(primitive) {
 /**
  * Pluck the IDs from any group layers within the provided array of primitives.
  *
- * @param {Primitive[]} primitives - An array of primitives
+ * @param {ImmutableMap[]} primitives - An array of primitives
  * @returns {Array} Array of group mark IDs
  */
 function getGroupIds(primitives) {
   return primitives.reduce(function(groupIds, primitive) {
     if (primitive.type === 'group') {
-      groupIds.push(primitive._id);
+      groupIds.push(primitive.get('_id'));
     }
     return groupIds;
   }, []);
@@ -55,11 +59,11 @@ function getGroupIds(primitives) {
  * Wrap a commonly chained sequence of hierarchy inquiries: take a primitive,
  * find all its parents, and return an array of those parents' IDs.
  *
- * @param {Object} primitive - A primitive for which to return parent layer IDs.
+ * @param {number} markId - The ID of a mark for which to return parent layer IDs.
  * @returns {number[]} An array of the (lyra) IDs of the primitive's parent layers.
  */
-function getParentGroupIds(primitive) {
-  return getGroupIds(getParents(primitive));
+function getParentGroupIds(markId) {
+  return getGroupIds(getParents(getIn(state, 'marks.' + markId)));
 }
 
 /**

src/js/components/InspectorSidebar.jsx
@@ -5,8 +5,7 @@
 var React = require('react'),
     Immutable = require('immutable'),
     connect = require('react-redux').connect,
-    model = require('../model'),
-    lookup = model.lookup,
+    store = require('../store'),
     Property = require('./inspectors/Property'),
     getIn = require('../util/immutable-utils').getIn,
     hierarchy = require('../util/hierarchy'),
@@ -52,7 +51,8 @@ var Inspector = React.createClass({
   render: function() {
     var props = this.props,
         prim = props.primitive && props.primitive.toJS(),
-        from = prim ? lookup(prim.from) : '',
+        from = prim && prim.from ?
+          getIn(store.getState(), 'datasets.' + prim.from.data) : '',
         ctor, InspectorType;
 
     if (prim) {

src/js/components/encodings/GroupSubMenu.jsx
@@ -2,8 +2,9 @@
 var React = require('react'),
     connect = require('react-redux').connect,
     ReactTooltip = require('react-tooltip'),
+    Immutable = require('immutable'),
+    store = require('../../store'),
     ContentEditable = require('../ContentEditable'),
-    lookup = require('../../model').lookup,
     get = require('../../util/immutable-utils').get,
     getIn = require('../../util/immutable-utils').getIn,
     selectMark = require('../../actions/inspectorActions').selectMark,
@@ -15,10 +16,11 @@ var React = require('react'),
     Icon = require('../Icon'),
     assets = require('../../util/assets');
 
-function mapStateToProps(reduxState) {
+function mapStateToProps(reduxState, ownProps) {
   return {
     selectedId: getIn(reduxState, 'inspector.encodings.selectedId'),
-    expandedLayers: getIn(reduxState, 'inspector.encodings.expandedLayers')
+    expandedLayers: getIn(reduxState, 'inspector.encodings.expandedLayers'),
+    group: getIn(reduxState, 'marks.' + ownProps.id)
   };
 }
 
@@ -34,12 +36,6 @@ function mapDispatchToProps(dispatch, ownProps) {
       dispatch(deleteMark(id));
     },
     updateProperty: function(id, property, value) {
-      // Update in the primitives dictionary
-      var mark = lookup(id);
-      if (mark) {
-        mark[property] = value;
-      }
-      // Update in the global store
       dispatch(updateMarkProperty(id, property, value));
     },
     toggle: function(layerId) {
@@ -57,7 +53,8 @@ var Group = React.createClass({
     select: React.PropTypes.func,
     deleteMark: React.PropTypes.func,
     updateProperty: React.PropTypes.func,
-    toggle: React.PropTypes.func
+    toggle: React.PropTypes.func,
+    group: React.PropTypes.instanceOf(Immutable.Map)
   },
 
   componentWillUnmount: function() {
@@ -85,18 +82,27 @@ var Group = React.createClass({
         level = +props.level,
         selectedId = props.selectedId,
         groupId = props.id,
-        group = lookup(groupId),
-        groupType = group.type,
-        isExpanded = get(props.expandedLayers, groupId);
+        group = props.group,
+        groupType = group.get('type'),
+        axes = group.get('axes'),
+        marks = group.get('marks'),
+        isExpanded = get(props.expandedLayers, groupId),
+        stored = store.getState();
 
-    var contents = isExpanded && group.marks ? (
+    var contents = isExpanded && group.get('marks') ? (
       <ul className="group">
-        <li className="header">Guides <Icon glyph={assets.plus} width="10" height="10" /></li>
+        <li className="header">Axes &amp; Legends <Icon glyph={assets.plus} width="10" height="10" /></li>
+
+        {axes.map(function(id) {
+          var axis = getIn(stored, 'guides.' + id);
+          return (<p key={id}>{axis.get('type')}</p>);
+        })}
+
           <li className="header">Marks <Icon glyph={assets.plus} width="10" height="10" /></li>
-        {group.marks.map(function(id) {
-          var mark = lookup(id),
-              type = mark.type,
-              name = mark.name,
+        {marks.map(function(id) {
+          var mark = getIn(stored, 'marks.' + id),
+              type = mark.get('type'),
+              name = mark.get('name'),
               icon = this.icon(type, isExpanded);
 
           return type === 'group' ? (
@@ -124,7 +130,7 @@ var Group = React.createClass({
     ) : null;
 
     var icon = this.icon(groupType, isExpanded),
-        name = group.name;
+        name = group.get('name');
 
     return (
       <li className={isExpanded ? 'expanded' : 'contracted'}>

src/js/components/encodings/LayerList.jsx
@@ -1,7 +1,6 @@
 'use strict';
 var React = require('react'),
     connect = require('react-redux').connect,
-    lookup = require('../../model').lookup,
     hierarchy = require('../../util/hierarchy'),
     getIn = require('../../util/immutable-utils').getIn,
     getClosestGroupId = require('../../util/store-utils').getClosestGroupId,
@@ -46,7 +45,7 @@ function mapDispatchToProps(dispatch) {
     },
     selectMark: function(id) {
       // Walk up from the selected primitive to create an array of its parent groups' IDs
-      var parentGroupIds = hierarchy.getParentGroupIds(lookup(id));
+      var parentGroupIds = hierarchy.getParentGroupIds(id);
 
       // Select the mark,
       dispatch(selectMark(id));

src/js/components/inspectors/Property.jsx
@@ -1,10 +1,9 @@
 'use strict';
 var React = require('react'),
     connect = require('react-redux').connect,
+    store = require('../../store'),
     SignalValue = require('../mixins/SignalValue'),
     getIn = require('../../util/immutable-utils').getIn,
-    model = require('../../model'),
-    lookup = model.lookup,
     resetMarkVisual = require('../../actions/markActions').resetMarkVisual;
 
 function mapStateToProps(state, ownProps) {
@@ -51,7 +50,8 @@ var Property = React.createClass({
   },
 
   render: function() {
-    var state = this.state,
+    var storedState = store.getState(),
+        state = this.state,
         props = this.props,
         name = props.name,
         label = props.label,
@@ -74,11 +74,11 @@ var Property = React.createClass({
     });
 
     labelEl = labelEl || (<label htmlFor={name}>{label}</label>);
-    scaleEl = scale && (scale = lookup(scale)) ?
-      (<div className="scale" onClick={this.unbind}>{scale.name}</div>) : null;
+    scaleEl = scale && (scale = getIn(storedState, 'scales.' + scale)) ?
+      (<div className="scale" onClick={this.unbind}>{scale.get('name')}</div>) : null;
 
-    controlEl = field && (field = lookup(field)) ?
-      (<div className="field" onClick={this.unbind}>{field._name}</div>) : controlEl;
+    controlEl = field ?
+      (<div className="field" onClick={this.unbind}>{field}</div>) : controlEl;
 
     if (!controlEl) {
       switch (type) {

src/js/components/inspectors/SpatialPreset.jsx
@@ -1,36 +1,59 @@
 'use strict';
 var React = require('react'),
-    propSg = require('../../util/prop-signal'),
-    model = require('../../model'),
-    lookup = model.lookup,
-    addVegaReparseRequest = require('../mixins/addVegaReparseRequest');
+    connect = require('react-redux').connect,
+    Immutable = require('immutable'),
+    addVegaReparseRequest = require('../mixins/addVegaReparseRequest'),
+    getIn = require('../../util/immutable-utils').getIn,
+    markActions = require('../../actions/markActions'),
+    setMarkVisual = markActions.setMarkVisual,
+    resetMarkVisual = markActions.resetMarkVisual;
+
+function mapStateToProps(reduxState, ownProps) {
+  var prim = ownProps.primitive,
+      name = ownProps.name,
+      update = prim.properties.update,
+      prop = update[name];
+
+  return {
+    property: update[name],
+    scale: getIn(reduxState, 'scales.' + prop.scale)
+  };
+}
+
+function mapDispatchToProps(dispatch, ownProps) {
+  var id = ownProps.primitive._id;
+  return {
+    setPreset: function(name, def) {
+      dispatch(setMarkVisual(id, name, def));
+    },
+    reset: function(name) {
+      dispatch(resetMarkVisual(id, name));
+    }
+  };
+}
 
 var SpatialPreset = React.createClass({
   propTypes: {
-    primitive: React.PropTypes.object
+    primitive: React.PropTypes.object,
+    property: React.PropTypes.object,
+    scale: React.PropTypes.instanceOf(Immutable.Map)
   },
 
   handleChange: function(evt) {
     var props = this.props,
-        primitive = props.primitive,
-        target = evt.target,
-        name = target.name,
-        update = primitive.properties.update,
-        prop = update[name],
-        scale = prop.scale && lookup(prop.scale),
+        name  = props.name,
+        scale = props.scale,
         preset = name.indexOf('x') >= 0 ? 'width' : 'height';
 
     if (evt.target.checked) {
-      update[name] = (name === 'width' || name === 'height') ? {
-        scale: scale,
+      props.setPreset(name, (name === 'width' || name === 'height') ? {
+        scale: scale.get('_id'),
         band: true
       } : {
         group: preset
-      };
+      });
     } else {
-      update[name] = {
-        signal: propSg(primitive, name)
-      };
+      props.reset(name);
     }
 
     this.requestVegaReparse();
@@ -38,21 +61,19 @@ var SpatialPreset = React.createClass({
 
   render: function() {
     var props = this.props,
-        name = props.name,
-        primitive = this.props.primitive,
-        update = primitive.properties.update,
-        prop = update[name],
-        scale = prop.scale && lookup(prop.scale),
+        name  = props.name,
+        scale = props.scale,
+        property = props.property,
         preset = name.indexOf('x') >= 0 ? 'width' : 'height';
 
-    if (prop.field) {
+    if (property.field) {
       return null;
     }
 
     if (name === 'width' || name === 'height') {
-      return (scale && scale.type === 'ordinal' && !scale.points) ? (
+      return (scale && scale.get('type') === 'ordinal' && !scale.get('points')) ? (
         <label>
-          <input type="checkbox" name={name} checked={prop.band}
+          <input type="checkbox" name={name} checked={property.band}
             onChange={this.handleChange} /> Automatic
         </label>
       ) : null;
@@ -60,11 +81,11 @@ var SpatialPreset = React.createClass({
 
     return (
       <label>
-        <input type="checkbox" name={name} checked={prop.group}
+        <input type="checkbox" name={name} checked={property.group}
           onChange={this.handleChange} /> Set to group {preset}
       </label>
     );
   }
 });
 
-module.exports = addVegaReparseRequest(SpatialPreset);
+module.exports = connect(mapStateToProps, mapDispatchToProps)(addVegaReparseRequest(SpatialPreset));

src/js/model/index.js
@@ -121,7 +121,7 @@ function register() {
     }
 
     // Walk up from the selected primitive to create an array of its parent groups' IDs
-    var parentLayerIds = hierarchy.getParentGroupIds(lookup(id));
+    var parentLayerIds = hierarchy.getParentGroupIds(id);
 
     if (id) {
       // Select the mark,

src/js/reducers/inspectorReducer.js
@@ -5,8 +5,7 @@ var Immutable = require('immutable'),
     immutils = require('../util/immutable-utils'),
     getIn = immutils.getIn,
     setIn = immutils.setIn,
-    ACTIONS = require('../actions/Names'),
-    hierarchy = require('../util/hierarchy');
+    ACTIONS = require('../actions/Names');
 
 function expandLayers(state, layerIds) {
   return layerIds.reduce(function(newState, layerId) {
@@ -43,8 +42,8 @@ function inspectorReducer(state, action) {
   }
 
   if (action.type === ACTIONS.SELECT_MARK) {
-    var lookup = require('../model').lookup,
-        parentGroupIds = hierarchy.getParentGroupIds(lookup(action.id));
+    var hierarchy = require('../util/hierarchy'),
+        parentGroupIds = hierarchy.getParentGroupIds(action.id);
 
     return expandLayers(state, parentGroupIds);
   }

commit 09ff1722cb973514de2a11f4263df2ceb29662c3

Fix bugs in hierarchy introduced in ec40dcb.

src/js/components/toolbar/AddMarks.jsx
@@ -5,7 +5,6 @@ var React = require('react'),
     Mark = require('../../store/factory/Mark'),
     getIn = require('../../util/immutable-utils').getIn,
     getClosestGroupId = require('../../util/store-utils').getClosestGroupId,
-    selectMark = require('../../actions/inspectorActions').selectMark,
     addMark = require('../../actions/markActions').addMark,
     assets = require('../../util/assets'),
     Icon = require('../Icon');
@@ -24,9 +23,6 @@ function mapDispatchToProps(dispatch) {
         _parent: parentId
       });
       dispatch(addMark(newMarkProps));
-    },
-    selectMark: function(id) {
-      dispatch(selectMark(id));
     }
   };
 }
@@ -38,8 +34,7 @@ var AddMarksTool = React.createClass({
   propTypes: {
     selectedId: React.PropTypes.number,
     sceneId: React.PropTypes.number,
-    addMark: React.PropTypes.func,
-    selectMark: React.PropTypes.func
+    addMark: React.PropTypes.func
   },
   classNames: 'new-marks',
   render: function() {

src/js/util/hierarchy.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var store = require('../store'),
-    state = store.getState(),
     getIn = require('./immutable-utils').getIn;
 
 /**
@@ -11,8 +10,7 @@ var store = require('../store'),
  * @returns {ImmutableMap|null} The requested mark, if present, else null
  */
 function getParent(mark) {
-  // Require ctrl in here to sidestep circular dependency issue
-  return getIn(state, 'marks.' + mark.get('_parent')) || null;
+  return getIn(store.getState(), 'marks.' + mark.get('_parent')) || null;
 }
 
 /**
@@ -48,7 +46,7 @@ function getParents(primitive) {
  */
 function getGroupIds(primitives) {
   return primitives.reduce(function(groupIds, primitive) {
-    if (primitive.type === 'group') {
+    if (primitive.get('type') === 'group') {
       groupIds.push(primitive.get('_id'));
     }
     return groupIds;
@@ -63,7 +61,7 @@ function getGroupIds(primitives) {
  * @returns {number[]} An array of the (lyra) IDs of the primitive's parent layers.
  */
 function getParentGroupIds(markId) {
-  return getGroupIds(getParents(getIn(state, 'marks.' + markId)));
+  return getGroupIds(getParents(getIn(store.getState(), 'marks.' + markId)));
 }
 
 /**