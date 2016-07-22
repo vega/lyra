63c241b366338caf3295edc68ae4b1457a0d72e5
Anjir Hossain <anjirh@uw.edu>
2016-07-22 12:36:22 -0700

    PipelineModal ui/ux wip

---
+++ src/js/util/debounce.js
@@ -0,0 +1,16 @@
+/* underscore debounce function */
+'use strict';
+module.exports = function(func, wait, immediate) {
+  var timeout;
+  return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
