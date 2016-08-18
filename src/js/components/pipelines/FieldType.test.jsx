16f423fe7d26c283be12e83cbaaf036ff226f030
mwc22 <mwc22@cs.washington.edu>
2016-08-17 20:41:41 -0700

    Deleted other tests because restarting. But finished testing FieldType

--- src/js/components/pipelines/FieldType.test.jsx
+++ src/js/components/pipelines/FieldType.test.jsx
@@ -5,21 +5,53 @@ var React = require('react'),
     expect = require('chai').expect,
     enzyme = require('enzyme'),
     shallow = enzyme.shallow,
-    DataTable = require('./FieldType'),
+    FieldType = require('./FieldType'),
+    measureTypes = require('../../constants/measureTypes'),
     wrapper;
 
-describe('FieldType Component <FieldType/>'), function() {
+describe('FieldType Component <FieldType/>', function() {
 
-  // Tests initial values of states
+  beforeEach(function() {
+    var field = { mtype: measureTypes[0] };
+    wrapper = shallow(<FieldType field={field} />);
+  });
+
   describe('Default state', function() {
 
-    beforeEach(function() {
-      wrapper = shallow(<FieldType />);
-    });
-
     it('valid init type', function() {
-      expect(wrapper.state('type').to.equal(null));
+      expect(wrapper.state('type')).to.be.null;
     });
 
   });
-}
+
+  // Renders component correctly
+  describe('Render', function() {
+
+    it('renders Icon', function() {
+      expect(wrapper.find('Icon')).to.have.length(1);
+    });
+
+  });
+
+  // Tests change type
+  describe('changeType', function() {
+
+    it('basic changeType', function() {
+      var prevType = wrapper.state('type');
+      wrapper.find('Icon').simulate('click');
+      var nextType = wrapper.state('type');
+      expect(prevType).to.not.equal(nextType);
+    });
+
+    it('changeType', function() {
+      wrapper.find('Icon').simulate('click');
+      var prevType = wrapper.state('type'),
+          prevIdx = measureTypes.indexOf(prevType),
+          corrType = measureTypes[(prevIdx + 1) % measureTypes.length];
+      wrapper.find('Icon').simulate('click');
+      var nextType = wrapper.state('type');
+      expect(corrType).to.equal(nextType);
+    });
+
+  });
+});
