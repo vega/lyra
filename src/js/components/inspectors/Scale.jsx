46feeb9485382df4447601e723b10883f15c4fa2
Yiyang <xu517@uw.edu>
2016-08-31 17:37:34 -0700

    add type properties

--- src/js/components/inspectors/Scale.jsx
+++ src/js/components/inspectors/Scale.jsx
@@ -38,12 +38,15 @@ var ScaleInspector = React.createClass({
   handleChange: function(prop, evt) {
     var scale = this.props.scale,
         points = getIn(scale, 'points'),
+        clamp = getIn(scale, 'clamp'),
         value = evt.target.value || '';
 
     if (prop === 'padding' || prop === 'exponent') {
       value = +value;
     } else if (prop === 'points') {
       value = !points;
+    } else if (prop === 'clamp') {
+      value = !clamp;
     }
 
     this.props.updateScaleProperty(this.props.primId, prop, value);
@@ -61,20 +64,63 @@ var ScaleInspector = React.createClass({
   render: function() {
     var scale = this.props.scale,
         points = getIn(scale, 'points'),
+        clamp = getIn(scale, 'clamp'),
         padding = getIn(scale, 'padding'),
+        nice = getIn(scale, 'nice'),
+        zero = getIn(scale, 'zero'),
         type = getIn(scale, 'type');
 
-    var typeSelections = (<Property lable="Type" type="select" name="type" opts={SCALE_TYPE} onChange={this.handleChange.bind(this, 'type')} />),
+    if (type === 'time' && typeof(nice) !== 'string') {
+      nice = 'second'; // default value
+    } else if ((type === 'linear' || type === 'log' || type === 'pow' || type === 'sqrt') && typeof(nice) !== 'boolean') {
+      nice = false; // default value
+    }
+
+    var typeSelections = (<Property label="Type" type="select" name="type" value={type} opts={SCALE_TYPE} onChange={this.handleChange.bind(this, 'type')} />),
+        quantitativeProps = (
+          <div>
+            <Property name="clamp" label="Clamp" type="checkbox" value={clamp} onChange={this.handleChange.bind(this, 'clamp')} />
+            <Property name="nice" label="Nice" type="checkbox" value={nice} onChange={this.handleChange.bind(this, 'nice')} />
+            <Property name="zero" label="Zero" type="checkbox" value={zero} onChange={this.handleChange.bind(this, 'zero')} />
+          </div>
+        ),
+
         scaleTypes;
 
         if (type === 'ordinal') {
           scaleTypes = (
             <div>
               {typeSelections}
-              <Property name="points" id="points" label="Points" type="checkbox" value={points} onChange={this.handleChange.bind(this, 'points')} />
+              <Property name="points" label="Points" type="checkbox" value={points} onChange={this.handleChange.bind(this, 'points')} />
               <Property name="padding" label="Padding" type="range" min="0" max="1" step="0.1" value={padding} onChange={this.handleChange.bind(this, 'padding')} />
             </div>
           );
+        } else if (type === 'time') {
+          scaleTypes = (
+            <div>
+              {typeSelections}
+              <Property name="clamp" label="Clamp" type="checkbox" value={clamp} onChange={this.handleChange.bind(this, 'clamp')} />
+              <Property label="Nice" type="select" name="nice" value={nice} opts={NICE_TIME} onChange={this.handleChange.bind(this, 'nice')} />
+            </div>
+          );
+
+        } else if (type === 'linear' || type === 'log' || type === 'sqrt') {
+          scaleTypes = (
+            <div>
+              {typeSelections}
+              {quantitativeProps}
+            </div>
+          );
+        } else if (type === 'pow') {
+          scaleTypes = (
+            <div>
+              {typeSelections}
+              <Property name='exponent' label='Exponent' type='number' onChange={this.handleChange.bind(this, 'pow')} />
+              {quantitativeProps}
+            </div>
+          );
+        } else {
+          scaleTypes = typeSelections;
         }
    
 
