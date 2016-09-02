9c6478828bb88ea92d13cfa1c01f0dc42ba4fcdf
Anjir Hossain <anjirh@uw.edu>
2016-09-02 14:04:27 -0700

    rebase lyra2

---
+++ src/js/components/walkthrough/ToolTip.jsx
@@ -0,0 +1,77 @@
+'use strict';
+var React = require('react'),
+    Icon = require('../Icon'),
+    close = require('../../util/assets').close;
+
+var ToolTip = React.createClass({
+  getInitialState: function() {
+    return {
+      position: {
+        x: -1000,
+        y: -1000
+      }
+    };
+  },
+  // componentWillMount: function() {
+  //   var props = this.props,
+  //       toolTipStyle = {},
+  //       targetDomEl,
+  //       boundingRect;
+  //
+  //   if (props && props.target) {
+  //     toolTipStyle.position = {};
+  //     // get targetDomEl position
+  //     targetDomEl = document.getElementById(props.target);
+  //     boundingRect = targetDomEl.getBoundingClientRect();
+  //
+  //     console.log('targetDomEl: ', targetDomEl);
+  //     // console.log(boundingRect.top, boundingRect.right, boundingRect.bottom, boundingRect.left);
+  //     // console.log(this.styles);
+  //   }
+  // },
+  componentWillReceiveProps: function(newProps) {
+    var props = this.props,
+        toolTipStyle = {},
+        targetDomEl,
+        boundingRect;
+
+    if (props && props.target) {
+      toolTipStyle.position = {};
+      // get targetDomEl position
+      targetDomEl = document.getElementById(props.target);
+      boundingRect = targetDomEl.getBoundingClientRect();
+
+      console.log('targetDomEl: ', targetDomEl);
+      console.log(boundingRect.top, boundingRect.right, boundingRect.bottom, boundingRect.left);
+      console.log(this.styles);
+    }
+  },
+  componentDidUpdate: function(newProps) {
+    console.log('newProps: ', newProps);
+  },
+  render: function() {
+    var props = this.props,
+        toolTipStyle;
+
+    return (
+      <div className="popover right in" role="tooltip">
+        <div className="arrow" style={toolTipStyle}></div>
+        <div className="popover-title">
+          <h3>{props.title}</h3>
+          <span className="close-hint" onClick={props.quit}>
+            <Icon glyph={close} />
+          </span>
+        </div>
+        <div className="popover-content">
+          {props.text}
+          {props.error}
+        </div>
+        <div className="popover-footer">
+          {props.control}
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = ToolTip;
---
+++ src/js/constants/exampleDatasets.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = [
+  {
+    name: 'Cars',
+    description: 'Vehicular data which consists of names, cylinders and displacement',
+    url: '/data/cars.json'
+  },
+  {
+    name: 'Jobs',
+    description: 'Job demographics such as job type, sex and count',
+    url:  '/data/jobs.json'
+  },
+  {
+    name: 'Gapminder',
+    description: 'Year, country, population, fertility',
+    url:  '/data/gapminder.json'
+  },
+  {
+    name: 'Climate',
+    description: 'Climate attributes such as temperature by lat-long',
+    url:  '/data/climate.json'
+  }
+];
