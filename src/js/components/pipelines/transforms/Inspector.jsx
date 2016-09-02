7826e53b4e0920afee870ee47bb53ccf47491706
mwc22 <mwc22@cs.washington.edu>
2016-09-01 23:05:14 -0700

    Started Inspector which will decide which transform inspector to use.

--- src/js/components/pipelines/transforms/Inspector.jsx
+++ src/js/components/pipelines/transforms/Inspector.jsx
@@ -1,4 +1,26 @@
 'use strict';
 var React = require('react'),
     Immutable = require('immutable'),
-    connect = require('react-redux').connect;
+    connect = require('react-redux').connect,
+    getInVis = imutils.getInVis,
+    ExpressionTextbox = require('./ExpressionTextbox');
+
+function mapStateToProps(state, ownProps) {
+  var id = ownProps.dsId;
+  return {
+    transforms: getInVis(state, 'datasets.' + id + '._transforms');
+  };
+}
+
+var Inspector = React.createClass({
+  propTypes: {
+    dsId:  React.PropTypes.number,
+    transforms:  React.PropTypes.array
+  },
+
+  render: function() {
+
+  }
+});
+
+module.exports = connect(mapStateToProps)(Inspector);
