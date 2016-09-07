c9a8fab5757d98976614b63684c943ae4d4af69d
Yiyang <xu517@uw.edu>
2016-09-07 16:39:22 -0700

    finish scale implements except drop box

--- src/js/components/inspectors/Scale.jsx
+++ src/js/components/inspectors/Scale.jsx
@@ -9,12 +9,7 @@ var React = require('react'),
     Property = require('./Property'),
     ScaleValueList = require('./ScaleValueList'),
     updateScaleProperty = require('../../actions/scaleActions').updateScaleProperty,
-    primTypes = require('../../constants/primTypes'),
-    assets = require('../../util/assets'),
-    Icon = require('../Icon');
-
-var SCALE_TYPE = ['linear', 'ordinal', 'time', 'utc', 'log', 'pow', 'sqrt', 'quantile', 'quantize', 'threshold'],
-    NICE_TIME = ['second', 'minute', 'hour', 'day', 'week', 'month', 'year'];
+    primTypes = require('../../constants/primTypes');
 
 function mapStateToProps(state, ownProps) {
   return {
@@ -27,7 +22,7 @@ function mapDispatchToProps(dispatch) {
     updateScaleProperty: function(scaleId, property, value) {
       dispatch(updateScaleProperty(scaleId, property, value));
     }
-  }
+  };
 }
 
 var ScaleInspector = React.createClass({
@@ -37,34 +32,61 @@ var ScaleInspector = React.createClass({
     scale: React.PropTypes.instanceOf(Immutable.Map)
   },
 
-  changeDomain: function() {
-    if (document.getElementById('domainF').checked) {
-      document.getElementById('fieldFeild').style.display = '';
-      document.getElementById('valuesField').style.display = 'none';
-    } else {
-      document.getElementById('valuesField').style.display = '';
-      document.getElementById('fieldFeild').style.display = 'none';
+  getInitialState: function() {
+    var domainPreset = true,
+        rangePreset = true,
+        rangeType = 'spatial';
+
+    return ({domainPreset: domainPreset,
+            rangePreset: rangePreset,
+            rangeType: rangeType});
+  },
+
+  setScaleState: function(state, value) {
+    if (state === 'domainPreset') {
+      this.setState({domainPreset: value});
+    } else if (state === 'rangePreset') {
+      this.setState({rangePreset: value});
     }
   },
 
-  handleChange: function(prop, evt) {
+  setRangeType: function(evt) {
+    var value = evt.target.value;
+    this.setState({rangeType: value});
+  },
+
+  handleValueChange: function(prop, value) {
+    this.props.updateScaleProperty(this.props.primId, prop, value);
+  },
+
+  handleBooleanChange: function(prop, evt) {
     var scale = this.props.scale,
         points = getIn(scale, 'points'),
         clamp = getIn(scale, 'clamp'),
         zero = getIn(scale, 'zero'),
         nice = getIn(scale, 'nice'),
-        value = evt.target.value;
+        value;
 
-    if (prop === 'padding' || prop === 'exponent') {
-      value = +value;
-    } else if (prop === 'points') {
+    if (prop === 'points') {
       value = !points;
     } else if (prop === 'clamp') {
       value = !clamp;
     } else if (prop === 'zero') {
       value = !zero;
-    } else if (prop === 'nice' && typeof(nice) === 'boolean' && value === 'on') {
+    } else if (prop === 'nice' && typeof (nice) === 'string') {
+      value = true;
+    } else if (prop === 'nice') {
       value = !nice;
+    }
+
+    this.props.updateScaleProperty(this.props.primId, prop, value);
+  },
+
+  handleChange: function(prop, evt) {
+    var value = evt.target.value;
+
+    if (prop === 'padding' || prop === 'exponent') {
+      value = +value;
     }
 
     this.props.updateScaleProperty(this.props.primId, prop, value);
@@ -78,63 +100,95 @@ var ScaleInspector = React.createClass({
         nice = getIn(scale, 'nice'),
         zero = getIn(scale, 'zero'),
         type = getIn(scale, 'type'),
-        domainValues = getIn(scale, '_domain');
+        rangePreset = this.state.rangePreset,
+        domainPreset = this.state.domainPreset,
+        rangeType = this.state.rangeType;
 
-    if (type === 'time' && typeof(nice) !== 'string') {
+    if (type === 'time' && typeof (nice) !== 'string') {
       nice = 'second'; // default value
-    } else if ((type === 'linear' || type === 'log' || type === 'pow' || type === 'sqrt') && typeof(nice) !== 'boolean') {
+    } else if ((type !== 'time') && typeof (nice) !== 'boolean') {
       nice = false; // default value
     }
 
-    var typeSelections = (<Property label="Type" type="select" name="type" value={type} opts={SCALE_TYPE} onChange={this.handleChange.bind(this, 'type')} />),
+    var scaleType = ['linear', 'ordinal', 'time', 'utc', 'log', 'pow', 'sqrt', 'quantile', 'quantize', 'threshold'],
+        niceTime = ['second', 'minute', 'hour', 'day', 'week', 'month', 'year'],
+        rangeTypes = ['spatial', 'shapes', 'colors', 'sizes', 'other'],
+        rangePresets = ['width', 'height', 'category10', 'category20', 'category20b', 'category20c'],
+        shapes = ['circle', 'cross', 'diamond', 'square', 'triangle-down', 'triangle-up'],
         quantitativeProps = (
           <div>
-            <Property name="clamp" label="Clamp" type="checkbox" value={clamp} onChange={this.handleChange.bind(this, 'clamp')} />
-            <Property name="nice" label="Nice" type="checkbox" value={nice} onChange={this.handleChange.bind(this, 'nice')} />
-            <Property name="zero" label="Zero" type="checkbox" value={zero} onChange={this.handleChange.bind(this, 'zero')} />
+            <Property name="clamp" label="Clamp" type="checkbox" value={clamp} onChange={this.handleBooleanChange.bind(this, 'clamp')} />
+            <Property name="nice" label="Nice" type="checkbox" value={nice} onChange={this.handleBooleanChange.bind(this, 'nice')} />
+            <Property name="zero" label="Zero" type="checkbox" value={zero} onChange={this.handleBooleanChange.bind(this, 'zero')} />
           </div>
         ),
-        styleShow = {display: ''},
-        styleHide = {display: 'none'},
+        domainFieldDisplay,
+        domainValueDisplay,
+        rangePresetDisplay,
+        rangeValueDisplay,
         scaleTypes,
-        scaleDomain;
+        rangeValues;
 
-        if (type === 'ordinal') {
-          scaleTypes = (
-            <div>
-              {typeSelections}
-              <Property name="points" label="Points" type="checkbox" value={points} onChange={this.handleChange.bind(this, 'points')} />
-              <Property name="padding" label="Padding" type="range" min="0" max="1" step="0.1" value={padding} onChange={this.handleChange.bind(this, 'padding')} />
-            </div>
-          );
-        } else if (type === 'time') {
-          scaleTypes = (
-            <div>
-              {typeSelections}
-              <Property name="clamp" label="Clamp" type="checkbox" value={clamp} onChange={this.handleChange.bind(this, 'clamp')} />
-              <Property label="Nice" type="select" name="nice" value={nice} opts={NICE_TIME} onChange={this.handleChange.bind(this, 'nice')} />
-            </div>
-          );
+    if (type === 'ordinal') {
+      scaleTypes = (
+        <div>
+          <Property name="points" label="Points" type="checkbox" value={points} onChange={this.handleBooleanChange.bind(this, 'points')} />
+          <Property name="padding" label="Padding" type="range" min="0" max="1" step="0.1" value={padding} onChange={this.handleChange.bind(this, 'padding')} />
+        </div>
+      );
+    } else if (type === 'time') {
+      scaleTypes = (
+        <div>
+          <Property name="clamp" label="Clamp" type="checkbox" value={clamp} onChange={this.handleBooleanChange.bind(this, 'clamp')} />
+          <Property label="Nice" type="select" name="nice" value={nice} opts={niceTime} onChange={this.handleChange.bind(this, 'nice')} />
+        </div>
+      );
 
-        } else if (type === 'linear' || type === 'log' || type === 'sqrt') {
-          scaleTypes = (
-            <div>
-              {typeSelections}
-              {quantitativeProps}
-            </div>
-          );
-        } else if (type === 'pow') {
-          scaleTypes = (
-            <div>
-              {typeSelections}
-              <Property name='exponent' label='Exponent' type='number' onChange={this.handleChange.bind(this, 'pow')} />
-              {quantitativeProps}
-            </div>
-          );
-        } else {
-          scaleTypes = typeSelections;
-        }
-        
+    } else if (type === 'linear' || type === 'log' || type === 'sqrt') {
+      scaleTypes = (
+        <div>
+          {quantitativeProps}
+        </div>
+      );
+    } else if (type === 'pow') {
+      scaleTypes = (
+        <div>
+          <Property name="exponent" label="Exponent" type="number" onChange={this.handleChange.bind(this, 'pow')} />
+          {quantitativeProps}
+        </div>
+      );
+    }
+
+    if (rangeType !== 'colors') {
+      rangeValues = (
+        <div>
+          <ScaleValueList scale={this.props.scale} scaleProp="range" propType="text" updateFn={this.handleValueChange.bind(this, 'range')}/>
+        </div>
+      );
+    } else {
+      rangeValues = (
+        <div>
+          <ScaleValueList scale={this.props.scale} scaleProp="range" propType="color" updateFn={this.handleValueChange.bind(this, 'range')}/>
+        </div>
+      );
+    }
+
+    if (domainPreset) {
+      domainFieldDisplay = {display: ''};
+      domainValueDisplay = {display: 'none'};
+    } else {
+      domainFieldDisplay = {display: 'none'};
+      domainValueDisplay = {display: ''};
+    }
+
+    if (rangePreset) {
+      rangePresetDisplay = {display: ''};
+      rangeValueDisplay = {display: 'none'};
+    } else {
+      rangePresetDisplay = {display: 'none'};
+      rangeValueDisplay = {display: ''};
+    }
+
     return (
       <div>
         <div className="property-group">
@@ -144,22 +198,22 @@ var ScaleInspector = React.createClass({
 
         <div className="property-group">
           <h3 className="label">Type</h3>
-            {scaleTypes}
+          <Property label="Type" type="select" name="type" value={type} opts={scaleType} onChange={this.handleChange.bind(this, 'type')} />
+          {scaleTypes}
         </div>
 
         <div className="property-group">
           <h3 className="label">Domain</h3>
           <div>
             From&nbsp;&nbsp;&nbsp;
-            <input type="radio" name="domainSelections" id="domainF" value="field" defaultChecked onClick={this.changeDomain.bind(this)} />field&nbsp;&nbsp;
-            <input type="radio" name="domainSelections" id="domainV" value="value" onClick={this.changeDomain.bind(this)} />value
+            <input type="radio" name="domainSelections" id="domainF" defaultChecked onClick={this.setScaleState.bind(this, 'domainPreset', true)} />field&nbsp;&nbsp;
+            <input type="radio" name="domainSelections" id="domainV" onClick={this.setScaleState.bind(this, 'domainPreset', false)} />value
           </div>
-          <div id='valuesField' style={styleHide}>
-            <p>value list</p>
-            <ScaleValueList scale={this.props.scale} scaleProp='domain' />
+          <div id="valuesField" style={domainValueDisplay}>
+            <ScaleValueList scale={this.props.scale} scaleProp="domain" propType="text" updateFn={this.handleValueChange.bind(this, 'domain')}/>
           </div>
-      
-          <div id='fieldFeild' style={styleShow}>
+
+          <div id="fieldFeild" style={domainFieldDisplay}>
             <p>drop field</p>
           </div>
 
@@ -167,6 +221,21 @@ var ScaleInspector = React.createClass({
 
         <div className="property-group">
           <h3 className="label">Range</h3>
+          <Property label="Type" type="select" name="type" opts={rangeTypes} onChange={this.setRangeType.bind(this)}/>
+          <div>
+            From&nbsp;&nbsp;&nbsp;
+            <input type="radio" name="rangeSelections" id="rangeP" defaultChecked onClick={this.setScaleState.bind(this, 'rangePreset', true)} />preset&nbsp;&nbsp;
+            <input type="radio" name="rangeSelections" id="rangeV" onClick={this.setScaleState.bind(this, 'rangePreset', false)} />value
+          </div>
+
+          <div id="rangePresetField" style={rangePresetDisplay}>
+            <p>drop field</p>
+          </div>
+
+          <div id="rangeValuesField" style={rangeValueDisplay}>
+            {rangeValues}
+          </div>
+
         </div>
       </div>
     );
--- src/js/components/inspectors/ScaleValueList.jsx
+++ src/js/components/inspectors/ScaleValueList.jsx
@@ -14,81 +14,114 @@ var React = require('react'),
 
 var ScaleValueList = React.createClass({
 	propTypes: {
-		//updateFn: React.PropTypes.func.isRequired,
+		updateFn: React.PropTypes.func.isRequired,
         scaleProp: React.PropTypes.string.isRequired, // should represent an array
-        scale: React.PropTypes.instanceOf(Immutable.Map).isRequired
+        scale: React.PropTypes.instanceOf(Immutable.Map).isRequired,
+        propType: React.PropTypes.string.isRequired
 	},
 
-    parseValue: function(val) {
-        if (isNaN(+val) || val === '') {
-            return val;
+    parseInValue: function(value) {
+        if (isNaN(+value) || value === '') {
+            return value;
         } else {
-            return +val;
+            return +value;
         }
     },
 
 	getInitialState: function() {
-		var frontValues = getIn(this.props.scale, this.props.scaleProp) || [],
-            lastValue = '';
-            
-    	return ({frontValues: frontValues,
-                lastValue: lastValue});
+		var values = [];
+        values.push('');
+        var valuesParsed = values.slice(0);
+    	return ({values: values,
+                valuesParsed: valuesParsed});
 	},
 
     handleValueChange: function(key, evt) {
-        var frontValues = this.state.frontValues,
-            value = this.parseValue(evt.target.value),
-            lastValue = this.state.lastValue;
+        var value = evt.target.value,
+            values = this.state.values,
+            valuesParsed = this.state.valuesParsed,
+            updateFn = this.props.updateFn;
 
-        if(key >= frontValues.length) {
-            lastValue = value;
-            this.setState({lastValue});
-        } else {
-            frontValues[key] = value;
-            this.setState({frontValues});
-        }      
+        console.log(values);
+        console.log(values.length);
+
+        values[key] = value;
+        this.setState({values: values});
+
+        valuesParsed[key] = this.parseInValue(value);
+        this.setState({valuesParsed: valuesParsed});
+        updateFn(valuesParsed);
     },
 
     addVal: function() {
-        var frontValues = this.state.frontValues,
-            lastValue = this.state.lastValue;
+        var values = this.state.values,
+            valuesParsed = this.state.valuesParsed;
 
-        frontValues.push(lastValue);
-        lastValue = '';
-
-        this.setState({frontValues});
-        this.setState({lastValue});
+        values.push('');
+        valuesParsed.push('');
+        this.setState({values});
+        this.setState({valuesParsed});
     },
 
     deleteVal: function(key) {
-        var frontValues = this.state.frontValues;
+        var values = this.state.values,
+            valuesParsed = this.state.valuesParsed,
+            updateFn = this.props.updateFn;
         
-        frontValues.splice(key, 1);
-        this.setState({frontValues});
+        values.splice(key, 1);
+        this.setState({values});
+
+        valuesParsed.splice(key, 1);
+        this.setState({valuesParsed});
+        
+        updateFn(valuesParsed);
     },
 
     render: function() {
-        var frontValues = this.state.frontValues,
-            lastValue = this.state.lastValue,
-            lastIndex = frontValues.length,
-            valueHTML = [];
+        var   values = this.state.values, 
+        lastIndex = values.length - 1,
+        lastValue = values[lastIndex],
+        propType = this.props.propType,
+            
+        valueHTML = [],
+        lastHTML;
 
-        for (var i = 0; i < frontValues.length; i++) {
-            var value = frontValues[i];
-            valueHTML.push(<div key={i}>
-                                <Property type='text' name='scaleProp' value={value} onChange={this.handleValueChange.bind(this, i)}/>
+        if (propType === 'text') {
+            for (var i = 0; i < lastIndex; i++) {
+                var value = values[i];
+                valueHTML.push(<div key={i}>
+                                <Property type='text' name='scaleProp' value={value} onBlur={this.handleValueChange.bind(this, i)}/>
                                 <Icon glyph={assets.close} width="12" height="12" onClick={this.deleteVal.bind(this, i)}/>
                             </div>);
+            }
+
+            lastHTML = (
+                <div key={lastIndex}>
+                    <Property type={propType} name='scaleProp' value={lastValue} onBlur={this.handleValueChange.bind(this, lastIndex)}/>
+                    <Icon glyph={assets.plus} width="12" height="12" onClick={this.addVal.bind(this, lastIndex)}/>
+                </div>
+            );
+        } else {
+            for (var i = 0; i < lastIndex; i++) {
+                var value = values[i];
+                valueHTML.push(<div key={i}>
+                                <Property type='color' name='scaleProp' value={value} onChange={this.handleValueChange.bind(this, i)}/>
+                                <Icon glyph={assets.close} width="12" height="12" onClick={this.deleteVal.bind(this, i)}/>
+                            </div>);
+            }
+            lastHTML = (
+                <div key={lastIndex}>
+                    <Property type='color' name='scaleProp' value={lastValue} onChange={this.handleValueChange.bind(this, lastIndex)}/>
+                    <Icon glyph={assets.plus} width="12" height="12" onClick={this.addVal.bind(this, lastIndex)}/>
+                </div>
+            );
         }
 
         return (
             <div>
                 {valueHTML}
-                <div key={lastIndex}>
-                    <Property type='text' name='scaleProp' value={lastValue} onChange={this.handleValueChange.bind(this, lastIndex)}/>
-                    <Icon glyph={assets.plus} width="12" height="12" onClick={this.addVal.bind(this, lastIndex)}/>
-                </div>
-            </div>
+                {lastHTML} 
+            </div>    
         );
 
     }
