3a8e81c4deee79eef2c07253a017f856f21b15b0
mwc22 <mwc22@cs.washington.edu>
2016-09-13 16:02:57 -0700

    Added bin the aggregateOps. Also verified that bin: true is being added to the Vega-Lite spec (parsed.input) in bindChannel

--- src/js/constants/aggregateOps.js
+++ src/js/constants/aggregateOps.js
@@ -3,5 +3,5 @@
 module.exports = [
   'mean', 'median', 'min', 'max', 'sum', 'count', 'distinct', 'valid',
   'missing', 'q1', 'q3', 'variance', 'variancep', 'stdev', 'stdevp',
-  'modeskew'
+  'modeskew', 'bin'
 ];
