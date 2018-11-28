59c56194ad6dfc458f19b450432b62fb3d7f0fad
Jonathan Zong <jonathan.zong@gmail.com>
2018-11-28 16:21:11 -0500

    change AddMarks to ts

--- src/js/components/toolbar/index.tsx
+++ src/js/components/toolbar/index.tsx
@@ -1,21 +1,21 @@
 import * as React from 'react';
+import { AddMarks } from './AddMarks';
 import { Export } from './Export';
 
-const AddMarks = require('./AddMarks'),
-  UndoRedo = require('./UndoRedo'),
+const UndoRedo = require('./UndoRedo'),
   assets = require('../../util/assets'),
   Icon = require('../Icon');
 
 export class Toolbar extends React.PureComponent {
   public render() {
     return (
-      <div className="toolbar">
-        <div className="toolbar-menu">
-          <input type="checkbox" id="nav-trigger" className="nav-trigger" />
-          <label htmlFor="nav-trigger">
+      <div className='toolbar'>
+        <div className='toolbar-menu'>
+          <input type='checkbox' id='nav-trigger' className='nav-trigger' />
+          <label htmlFor='nav-trigger'>
             <Icon glyph={assets.hamburger} />
           </label>
-          <div className="menu">
+          <div className='menu'>
             <AddMarks />
             <UndoRedo />
             <Export />
