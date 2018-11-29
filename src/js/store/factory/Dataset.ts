57ff9636e1b23fab0589f0ba281491f7072508e5
Arvind Satyanarayan <arvindsatya1@gmail.com>
2018-11-29 12:58:18 -0500

    Switch Dataset to Record type.

--- src/js/store/factory/Dataset.ts
+++ src/js/store/factory/Dataset.ts
@@ -1,34 +1,11 @@
-import {Record} from 'immutable';
+import {Map, Record, RecordOf} from 'immutable';
+import {Data} from 'vega-typings';
 
 /**
  * A factory to produce a Lyra dataset. Each dataset corresponds to a single
  * definition of a data source in the resultant Vega specification. Pipelines,
  * on the other hand, may contain more than one dataset.
- *
- * @see  Vega's {@link https://github.com/vega/vega/wiki/Data|Data source}
- * documentation for more information on this class' "public" properties.
  */
-interface LyraDataset {
-  // The Lyra ID of this dataset.
-  _id: number;
-
-  // The name of the dataset.
-  name: string;
-
-  // name of another dataset to use as the source dataset
-  source: string;
-
-  // url from which to load the dataset
-  url: string;
-
-  // object that specifies the data format (e.g. json, csv, ...)
-  format: {parse: string, type: string};
-}
-
-export const Dataset = Record<LyraDataset>({
-  _id: null,
-  name: null,
-  source: null,
-  url: null,
-  format: null
-});
+export const Dataset = Record<Data>({name: ''});
+export type DatasetRecord = RecordOf<Data>;
+export type DatasetState = Map<number, DatasetRecord>;
