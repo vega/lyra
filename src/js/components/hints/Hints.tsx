2f06df1753d64f39588fae7e66b0ca149bd3b81f
Admin <arlu@umich.edu>
2018-12-05 15:06:47 -0500

    Change Hints from jsx to tsx

--- src/js/components/hints/Hints.tsx
+++ src/js/components/hints/Hints.tsx
@@ -1,31 +1,23 @@
 'use strict';
-const getIn = require('../../util/immutable-utils').getIn;
-const hintActions = require('../../actions/hintActions');
-const assets = require('../../util/assets');
-const Icon = require('../Icon');
+var React = require('react'),
+    connect = require('react-redux').connect,
+    getIn = require('../../util/immutable-utils').getIn,
+    hintActions = require('../../actions/hintActions'),
+    assets = require('../../util/assets'),
+    Icon = require('../Icon'),
+    propTypes = require('prop-types'),
+    createReactClass = require('create-react-class');
 
-import * as React from 'react';
-import {connect} from 'react-redux';
-import { Dispatch } from 'redux';
-import {State} from '../../store';
-
-interface HintsProps {
-  displayHint: any,
-  dispatchAction: () => any,
-  clearHints: () => any
-}
-
-function mapStateToProps(reduxState: State, ownProps: HintsProps) {
+function mapStateToProps(reduxState, ownProps) {
   return {
     displayHint: getIn(reduxState, 'hints.display')
   };
 }
-
-function mapDispatchToProps(dispatch: Dispatch, ownProps: HintsProps) {
+function mapDispatchToProps(dispatch, ownProps) {
   return {
     dispatchAction: function() {
-      const action = this.props.displayHint.action;
-      const actionProps = this.props.displayHint.action_props !== undefined ?
+      var action = this.props.displayHint.action;
+      var actionProps = this.props.displayHint.action_props !== undefined ?
         this.props.displayHint.action_props : '';
       dispatch(action(actionProps));
       dispatch(hintActions.clearHints());
@@ -36,22 +28,27 @@ function mapDispatchToProps(dispatch: Dispatch, ownProps: HintsProps) {
   };
 }
 
-class BaseHints extends React.Component<HintsProps> {
-  public classNames: 'hints';
-  public render() {
-    const hint = this.props.displayHint;
+var Hints = createReactClass({
+  propTypes: {
+    displayHint: propTypes.object,
+    dispatchAction: propTypes.func,
+    clearHints: propTypes.func
+  },
+  classNames: 'hints',
+  render: function() {
+    var hint = this.props.displayHint;
     // If there is an action in the displayHint object, show the action button.
-    const action = hint.action ? (
-        <a className='action button button-secondary' onClick={this.props.dispatchAction.bind(this, '')}>
+    var action = hint.action ? (
+        <a className="action button button-secondary" onClick={this.props.dispatchAction.bind(this, '')}>
           {hint.action_text}
         </a>
       ) : '';
     // Content is dependent on if the hint template exists
-    const Template = hint.template;
-    const content = Template ? (<Template/>) :
+    var Template = hint.template;
+    var content = Template ? (<Template/>) :
       (
         <div>
-          <h4 className='hint-header'>{hint.title}</h4>
+          <h4 className="hint-header">{hint.title}</h4>
           <p>
             {hint.text}
           </p>
@@ -63,15 +60,12 @@ class BaseHints extends React.Component<HintsProps> {
       <div className={this.classNames}>
         {content}
         {action}
-        <span className='close-hint' onClick={this.props.clearHints.bind(null, '')}>
+        <span className="close-hint" onClick={this.props.clearHints.bind(null, '')}>
           <Icon glyph={assets.close} />
         </span>
       </div>
     );
-  };
-};
+  }
+});
 
-export const Hints = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(BaseHints);
+module.exports = connect(mapStateToProps, mapDispatchToProps)(Hints);
