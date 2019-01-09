80947ff76029e9dab22c31a0b43862aaa79558a5
Jonathan Zong <jonathan.zong@gmail.com>
2019-01-09 16:42:04 -0500

    grouplist to tsx

--- src/js/components/encodings/GroupList.tsx
+++ src/js/components/encodings/GroupList.tsx
@@ -1,21 +1,32 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
+import {State} from '../../store';
 import Group from './GroupChildren';
 
-'use strict';
-const React = require('react'),
-    connect = require('react-redux').connect,
-    imutils = require('../../util/immutable-utils'),
-    getIn = imutils.getIn,
-    getInVis = imutils.getInVis,
-    Mark = require('../../store/factory/Mark'),
-    addMark = require('../../actions/markActions').addMark,
-    selectMark = require('../../actions/inspectorActions').selectMark,
-    clearScene = require('../../actions/sceneActions').clearScene,
-    assets = require('../../util/assets'),
-    Icon = require('../Icon'),
-    propTypes = require('prop-types'),
-    createReactClass = require('create-react-class');
+const imutils = require('../../util/immutable-utils');
+const getIn = imutils.getIn;
+const getInVis = imutils.getInVis;
+const Mark = require('../../store/factory/Mark');
+const addMark = require('../../actions/markActions').addMark;
+const selectMark = require('../../actions/inspectorActions').selectMark;
+const clearScene = require('../../actions/sceneActions').clearScene;
+const assets = require('../../util/assets');
+const Icon = require('../Icon');
 
-function mapStateToProps(reduxState) {
+interface StateProps {
+  sceneId: number;
+  selectedId: number;
+  marks: any;
+}
+
+interface DispatchProps {
+  addMark: (type, parentId) => void;
+  selectMark: (id) => void;
+  clearScene: (event) => void;
+}
+
+function mapStateToProps(reduxState: State): StateProps {
   const sceneId = getInVis(reduxState, 'scene.id');
   return {
     sceneId: sceneId,
@@ -24,7 +35,7 @@ function mapStateToProps(reduxState) {
   };
 }
 
-function mapDispatchToProps(dispatch) {
+function mapDispatchToProps(dispatch: Dispatch): DispatchProps {
   return {
     addMark: function(type, parentId) {
       dispatch(addMark(Mark(type, {_parent: parentId})));
@@ -39,21 +50,13 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
-const GroupList = createReactClass({
-  propTypes: {
-    selectedId: propTypes.number,
-    sceneId: propTypes.number,
-    marks: propTypes.object,
-    addMark: propTypes.func,
-    selectMark: propTypes.func,
-    clearScene: propTypes.func
-  },
+class GroupList extends React.Component<StateProps & DispatchProps> {
 
-  render: function() {
-    const props = this.props,
-        sceneId = props.sceneId,
-        sceneSelected = props.selectedId === sceneId,
-        groups = props.marks ? props.marks : [];
+  public render() {
+    const props = this.props;
+    const sceneId = props.sceneId;
+    const sceneSelected = props.selectedId === sceneId;
+    const groups = props.marks ? props.marks : [];
 
     return (
       <div id='layer-list' className='expandingMenu'>
@@ -84,6 +87,6 @@ const GroupList = createReactClass({
       </div>
     );
   }
-});
+}
 
-module.exports = connect(mapStateToProps, mapDispatchToProps)(GroupList);
+export default connect(mapStateToProps, mapDispatchToProps)(GroupList);
