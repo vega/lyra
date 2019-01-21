c915bbf46f087b5ea075032d664822aa32f7527e
Admin <arlu@umich.edu>
2019-01-21 11:56:28 -0500

    Refactor Line.tsx

--- src/js/components/inspectors/Line.tsx
+++ src/js/components/inspectors/Line.tsx
@@ -1,49 +1,49 @@
 'use strict';
-var React = require('react'),
-    Property = require('./Property'),
-    primTypes = require('../../constants/primTypes'),
-    INTERPOLATE = require('../../constants/interpolate'),
-    propTypes = require('prop-types'),
-    createReactClass = require('create-react-class');
+const Property = require('./Property');
+const primTypes = require('../../constants/primTypes');
+const INTERPOLATE = require('../../constants/interpolate');
 
-var LineInspector = createReactClass({
-  propTypes: {
-    primId: propTypes.number.isRequired,
-    primType: primTypes.isRequired
-  },
-  render: function() {
-    var props = this.props;
+import * as React from 'react';
+
+interface LineInspectorProps {
+  primId: number;
+  primType: 'marks'|'guides'|'scales'|any; // TODO: Should import from primType: primTypes.isRequired
+
+}
+class BaseLineInspector extends React.Component<LineInspectorProps> {
+  public render() {
+    const props = this.props;
     return (
       <div>
-        <div className="property-group">
+        <div className='property-group'>
           <h3>Position</h3>
 
-          <Property name="x" label="X" type="number" canDrop={true} {...props} />
+          <Property name='x' label='X' type='number' canDrop={true} {...props} />
 
-          <Property name="y" label="Y" type="number" canDrop={true} {...props} />
+          <Property name='y' label='Y' type='number' canDrop={true} {...props} />
         </div>
 
-        <div className="property-group">
+        <div className='property-group'>
           <h3>Stroke</h3>
 
-          <Property name="stroke" label="Color" type="color" canDrop={true} {...props} />
+          <Property name='stroke' label='Color' type='color' canDrop={true} {...props} />
 
-          <Property name="strokeWidth" label="Width" type="range"
-            min="0" max="10" step="0.25" canDrop={true} {...props} />
+          <Property name='strokeWidth' label='Width' type='range'
+            min='0' max='10' step='0.25' canDrop={true} {...props} />
         </div>
 
-        <div className="property-group">
+        <div className='property-group'>
           <h3>Line Strength</h3>
 
-          <Property name="interpolate" label="Interpolate" type="select"
+          <Property name='interpolate' label='Interpolate' type='select'
             opts={INTERPOLATE} canDrop={true} {...props} />
 
-          <Property name="tension" label="Tension" type="number"
+          <Property name='tension' label='Tension' type='number'
             canDrop={true} {...props} />
         </div>
       </div>
     );
   }
-});
+};
 
-module.exports = LineInspector;
+export const LineInspector = BaseLineInspector;
