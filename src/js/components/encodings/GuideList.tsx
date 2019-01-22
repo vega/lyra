ade8ec1b30f9b89259b92d42cf648c304bda8265
Jonathan Zong <jonathan.zong@gmail.com>
2019-01-22 15:36:51 -0500

    refactor encodings/GuideList

--- src/js/components/encodings/GuideList.tsx
+++ src/js/components/encodings/GuideList.tsx
@@ -1,26 +1,24 @@
-'use strict';
+import * as React from 'react';
+import { connect } from 'react-redux';
+import * as ReactTooltip from 'react-tooltip';
+import { Dispatch } from 'redux';
+import {State} from '../../store';
 
-var React = require('react'),
-    connect = require('react-redux').connect,
-    Immutable = require('immutable'),
-    capitalize = require('capitalize'),
-    ReactTooltip = require('react-tooltip'),
-    inspectorActions = require('../../actions/inspectorActions'),
-    selectMark = inspectorActions.selectMark,
-    selectGuide = inspectorActions.selectGuide,
-    deleteGuide = require('../../actions/guideActions').deleteGuide,
-    imutils = require('../../util/immutable-utils'),
-    getIn = imutils.getIn,
-    getInVis = imutils.getInVis,
-    Icon = require('../Icon'),
-    assets = require('../../util/assets'),
-    propTypes = require('prop-types'),
-    createReactClass = require('create-react-class');
+const capitalize = require('capitalize');
+const inspectorActions = require('../../actions/inspectorActions');
+const selectMark = inspectorActions.selectMark;
+const selectGuide = inspectorActions.selectGuide;
+const deleteGuide = require('../../actions/guideActions').deleteGuide;
+const imutils = require('../../util/immutable-utils');
+const getIn = imutils.getIn;
+const getInVis = imutils.getInVis;
+const Icon = require('../Icon');
+const assets = require('../../util/assets');
 
-function mapStateToProps(reduxState, ownProps) {
-  var groupId = ownProps.groupId,
-      axes = getInVis(reduxState, 'marks.' + groupId + '.axes'),
-      legends = getInVis(reduxState, 'marks.' + groupId + '.legends');
+function mapStateToProps(reduxState: State, ownProps: GuideListProps) {
+  const groupId = ownProps.groupId;
+  const axes = getInVis(reduxState, 'marks.' + groupId + '.axes');
+  const legends = getInVis(reduxState, 'marks.' + groupId + '.legends');
 
   return {
     scales: getInVis(reduxState, 'scales'),
@@ -32,13 +30,13 @@ function mapStateToProps(reduxState, ownProps) {
   };
 }
 
-function mapDispatchToProps(dispatch, ownProps) {
+function mapDispatchToProps(dispatch: Dispatch, ownProps: GuideListProps) {
   return {
     selectGuide: function(guideId) {
       dispatch(selectGuide(guideId));
     },
     deleteGuide: function(selectedId, guideId, evt) {
-      var groupId = ownProps.groupId;
+      const groupId = ownProps.groupId;
       if (selectedId === guideId) {
         dispatch(selectMark(groupId));
       }
@@ -50,35 +48,35 @@ function mapDispatchToProps(dispatch, ownProps) {
   };
 }
 
-var GuideList = createReactClass({
-  propTypes: {
-    groupId: propTypes.number.isRequired,
-    selectedId: propTypes.number,
-    scales: propTypes.instanceOf(Immutable.Map),
-    guides: propTypes.instanceOf(Immutable.List),
-    selectGuide: propTypes.func.isRequired,
-    deleteGuide: propTypes.func.isRequired
-  },
+interface GuideListProps {
+  groupId: number;
+  selectedId: number;
+  scales?: any; // Immutable.Map
+  guides?: any; // Immutable.List
+  selectGuide: () => any;
+  deleteGuide: () => any;
+}
 
-  componentDidUpdate: function() {
+class GuideList extends React.Component<GuideListProps> {
+  public componentDidUpdate() {
     ReactTooltip.rebuild();
-  },
+  }
 
-  render: function() {
-    var props = this.props,
-        selectedId = props.selectedId;
+  public render() {
+    const props = this.props;
+    const selectedId = props.selectedId;
 
     return (
       <div>
-        <li className="header">
-          Guides <Icon glyph={assets.plus} width="10" height="10" />
+        <li className='header'>
+          Guides <Icon glyph={assets.plus} width='10' height='10' />
         </li>
 
         {props.guides.map(function(guide) {
-          var guideId = guide.get('_id'),
-              scaleId = guide.get('scale') || guide.get(guide.get('_type')),
-              name = capitalize(getIn(props.scales, scaleId + '.name')),
-              type = capitalize(guide.get('_gtype'));
+          const guideId = guide.get('_id');
+          const scaleId = guide.get('scale') || guide.get(guide.get('_type'));
+          const name = capitalize(getIn(props.scales, scaleId + '.name'));
+          const type = capitalize(guide.get('_gtype'));
 
           return (
             <li key={guideId}>
@@ -87,9 +85,9 @@ var GuideList = createReactClass({
 
                 {name + ' ' + type}
 
-                <Icon glyph={assets.trash} className="delete"
+                <Icon glyph={assets.trash} className='delete'
                   onClick={props.deleteGuide.bind(null, selectedId, guideId)}
-                  data-tip={'Delete ' + name + ' ' + type} data-place="right" />
+                  data-tip={'Delete ' + name + ' ' + type} data-place='right' />
               </div>
             </li>
           );
@@ -97,6 +95,6 @@ var GuideList = createReactClass({
       </div>
     );
   }
-});
+}
 
-module.exports = connect(mapStateToProps, mapDispatchToProps)(GuideList);
+export default connect(mapStateToProps, mapDispatchToProps)(GuideList);
