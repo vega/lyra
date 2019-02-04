8d7a43e25959b99c77030c3bfcfd276447bf4724
Admin <arlu@umich.edu>
2019-02-04 18:56:42 -0500

    Refactor Symbol.tsx

--- src/js/components/inspectors/Symbol.tsx
+++ src/js/components/inspectors/Symbol.tsx
@@ -1,60 +1,59 @@
 'use strict';
-var React = require('react'),
-    Property = require('./Property'),
-    primTypes = require('../../constants/primTypes'),
-    Symbol = require('../../store/factory/marks/Symbol'),
-    propTypes = require('prop-types'),
-    createReactClass = require('create-react-class');
+const Property = require('./Property');
+const primTypes = require('../../constants/primTypes');
+const Symbol = require('../../store/factory/marks/Symbol');
 
-var SymbolInspector = createReactClass({
-  propTypes: {
-    primId: propTypes.number.isRequired,
-    primType: primTypes.isRequired
-  },
+import * as React from 'react';
 
-  render: function() {
-    var props = this.props;
+interface SymbolInspectorProps {
+  primId: number,
+  primType: any
+}
+
+class BaseSymbolInspector extends React.Component<SymbolInspectorProps> {
+  public render() {
+    const props = this.props;
     return (
       <div>
-        <div className="property-group">
+        <div className='property-group'>
           <h3>Position</h3>
 
-          <Property name="x" label="X" type="number" canDrop={true} {...props} />
+          <Property name='x' label='X' type='number' canDrop={true} {...props} />
 
-          <Property name="y" label="Y" type="number" canDrop={true} {...props} />
+          <Property name='y' label='Y' type='number' canDrop={true} {...props} />
         </div>
 
-        <div className="property-group">
+        <div className='property-group'>
           <h3>Geometry</h3>
 
-          <Property name="shape" label="Shape" type="select" opts={Symbol.SHAPES}
+          <Property name='shape' label='Shape' type='select' opts={Symbol.SHAPES}
             canDrop={true} {...props} />
 
-          <Property name="size" label="Size" type="number" canDrop={true} {...props} />
+          <Property name='size' label='Size' type='number' canDrop={true} {...props} />
 
         </div>
 
-        <div className="property-group">
+        <div className='property-group'>
           <h3>Fill</h3>
 
-          <Property name="fill" label="Color" type="color" canDrop={true} {...props} />
+          <Property name='fill' label='Color' type='color' canDrop={true} {...props} />
 
-          <Property name="fillOpacity" label="Opacity" type="range"
-            min="0" max="1" step="0.05" canDrop={true} {...props} />
+          <Property name='fillOpacity' label='Opacity' type='range'
+            min='0' max='1' step='0.05' canDrop={true} {...props} />
 
         </div>
 
-        <div className="property-group">
+        <div className='property-group'>
           <h3>Stroke</h3>
 
-          <Property name="stroke" label="Color" type="color" canDrop={true} {...props} />
+          <Property name='stroke' label='Color' type='color' canDrop={true} {...props} />
 
-          <Property name="strokeWidth" label="Width" type="range"
-            min="0" max="10" step="0.25" canDrop={true} {...props} />
+          <Property name='strokeWidth' label='Width' type='range'
+            min='0' max='10' step='0.25' canDrop={true} {...props} />
         </div>
       </div>
     );
   }
-});
+};
 
-module.exports = SymbolInspector;
+export const SymbolInspector = BaseSymbolInspector;
