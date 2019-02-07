55e2a2643253a2f6c326311494f3bb06ea3dfefc
Jonathan Zong <jonathan.zong@gmail.com>
2019-02-07 15:43:46 -0500

    type Text mark

--- src/js/store/factory/marks/Text.ts
+++ src/js/store/factory/marks/Text.ts
@@ -5,28 +5,30 @@ const anchorTarget = require('../../../util/anchor-target');
 const test = require('../../../util/test-if');
 const propSg = require('../../../util/prop-signal');
 
-/**
- * A text mark factory.
- * @returns {Object} Additional default visual properties for a text mark.
- */
-function Text() {
-  return {
-    properties: {
-      update: {
-        dx: {value: 0, offset: 0},
-        dy: {value: 0, offset: 0},
-        text: {template: 'Text'},
-        align: {value: 'center'},
-        baseline: {value: 'middle'},
-        font: {value: 'Helvetica'},
-        fontSize: {value: 14},
-        fontStyle: {value: 'normal'},
-        fontWeight: {value: 'normal'},
-        angle: {value: 0}
-      }
+export type LyraTextMark = {
+  _id: number
+} & TextMark;
+
+export const Text = Record<LyraTextMark>({
+  _id: null,
+  type: 'text',
+  encode: {
+    update: {
+      dx: {value: 0, offset: 0},
+      dy: {value: 0, offset: 0},
+      text: {value: 'Text'},
+      align: {value: 'center'},
+      baseline: {value: 'middle'},
+      font: {value: 'Helvetica'},
+      fontSize: {value: 14},
+      fontStyle: {value: 'normal'},
+      fontWeight: {value: 'normal'},
+      angle: {value: 0}
     }
-  };
-}
+  }
+});
+
+export type TextRecord = RecordOf<LyraTextMark>;
 
 /**
  * Return an array of handle signal stream definitions to be instantiated.
@@ -40,57 +42,30 @@ function Text() {
  * @param {string} text.type - A mark type, presumably "text"
  * @returns {Object} A dictionary of stream definitions keyed by signal name
  */
-Text.getHandleStreams = function(text) {
-  const sg = require('../../../ctrl/signals'),
-      at = anchorTarget.bind(null, text, 'handles'),
-      id = text._id,
-      x = propSg(id, 'text', 'x'),
-      y = propSg(id, 'text', 'y'),
-      fontSize = propSg(id, 'text', 'fontSize'),
-      DELTA = sg.DELTA,
-      DX = DELTA + '.x',
-      DY = DELTA + '.y',
-      streams = {};
+export function getHandleStreams(text: TextRecord): {[s: string]: OnEvent[];} {
+  const sg = require('../../../ctrl/signals');
+  const at = anchorTarget.bind(null, text, 'handles');
+  const id = text._id;
+  const x = propSg(id, 'text', 'x');
+  const y = propSg(id, 'text', 'y');
+  const fontSize = propSg(id, 'text', 'fontSize');
+  const DELTA = sg.DELTA;
+  const DX = DELTA + '.x';
+  const DY = DELTA + '.y';
+  const streams: {[s: string]: OnEvent[];} = {};
 
   streams[x] = [{
-    type: DELTA, expr: test(at(), x + '+' + DX, x)
+    events: DELTA, update: test(at(), x + '+' + DX, x)
   }];
   streams[y] = [{
-    type: DELTA, expr: test(at(), y + '+' + DY, y)
+    events: DELTA, update: test(at(), y + '+' + DY, y)
   }];
   // Allow upper-left and lower-right handles to control font size
   streams[fontSize] = [
-    {type: DELTA, expr: test(at('left') + '&&' + at('top'), fontSize + '-' + DX, fontSize)},
-    {type: DELTA, expr: test(at('right') + '&&' + at('bottom'), fontSize + '+' + DX, fontSize)},
-    {type: DELTA, expr: test(at('left') + '&&' + at('top'), fontSize + '-' + DY, fontSize)},
-    {type: DELTA, expr: test(at('right') + '&&' + at('bottom'), fontSize + '+' + DY, fontSize)}
+    {events: DELTA, update: test(at('left') + '&&' + at('top'), fontSize + '-' + DX, fontSize)},
+    {events: DELTA, update: test(at('right') + '&&' + at('bottom'), fontSize + '+' + DX, fontSize)},
+    {events: DELTA, update: test(at('left') + '&&' + at('top'), fontSize + '-' + DY, fontSize)},
+    {events: DELTA, update: test(at('right') + '&&' + at('bottom'), fontSize + '+' + DY, fontSize)}
   ];
   return streams;
 };
-
-/**
- * @property alignments {string[]} Valid align properties for vega text marks
- */
-Text.alignments = ['left', 'center', 'right'];
-
-/**
- * @property baselines {string[]} Valid baseline properties for vega text marks
- */
-Text.baselines = ['top', 'middle', 'bottom'];
-
-/**
- * @property fonts {string[]} Valid fonts for vega text marks
- */
-Text.fonts = ['Helvetica', 'Verdana', 'Georgia', 'Palatino', 'Garamond', 'Trebuchet MS'];
-
-/**
- * @property fontStyles {string[]} Valid font styles for vega text marks
- */
-Text.fontStyles = ['normal', 'italic'];
-
-/**
- * @property fontWeights {string[]} Valid font weights for vega text marks
- */
-Text.fontWeights = ['normal', 'bold'];
-
-module.exports = Text;
