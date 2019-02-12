6a42fbabc12937849f5a90d10e9c1e66fafaef79
Admin <arlu@umich.edu>
2019-02-11 21:55:41 -0500

    Update scalesReducer

--- src/js/reducers/scalesReducer.ts
+++ src/js/reducers/scalesReducer.ts
@@ -1,39 +1,43 @@
 /* eslint new-cap:0 */
 'use strict';
 
-const ACTIONS = require('../actions/Names');
 const immutableUtils = require('../util/immutable-utils');
 const getIn = immutableUtils.getIn;
 const set = immutableUtils.set;
+const str = immutableUtils.str;
 const setIn = immutableUtils.setIn;
 const deleteKeyFromMap = immutableUtils.deleteKeyFromMap;
 
 import {fromJS, Map} from 'immutable';
-import {getType} from 'typesafe-actions';
+import {ActionType, getType} from 'typesafe-actions';
 import * as scaleActions from '../actions/scaleActions';
 import {ScaleState} from '../store/factory/Scale';
 
-export function scalesReducer(state: ScaleState, action): ScaleState {
+export function scalesReducer(state: ScaleState, action: ActionType<typeof scaleActions>): ScaleState {
+  const id = action.meta;
+
   if (typeof state === 'undefined') {
     return Map();
   }
 
   if (action.type === getType(scaleActions.addScale)) {
-    return set(state, action.id, fromJS(action.props));
+    return set(str(id), action.payload);
   }
 
   if (action.type === getType(scaleActions.updateScaleProperty)) {
-    return setIn(state, action.id + '.' + action.property,
-      fromJS(action.value));
+    const p = action.payload;
+    return setIn(state, str(id) + '.' + p.property,
+      fromJS(p.value));
   }
 
   if (action.type === getType(scaleActions.amendDataRef)) {
-    const refs = getIn(state, action.id + '.' + action.property);
-    return setIn(state, action.id + '.' + action.property, refs.push(action.ref));
+    const p = action.payload;
+    const refs = getIn(state, str(id) + '.' + p.property);
+    return setIn(state, str(id) + '.' + p.property, refs.push(p.ref));
   }
 
   if (action.type === getType(scaleActions.deleteScale)) {
-    return deleteKeyFromMap(state, action.id);
+    return deleteKeyFromMap(state, str(id));
   }
 
   return state;
