9f57cb8dd7dd4fcb02d8c1672a47e6faac4511c5
Jonathan Zong <jonathan.zong@gmail.com>
2019-02-14 11:21:21 -0500

    refactor vegaActions

--- src/js/actions/vegaActions.ts
+++ src/js/actions/vegaActions.ts
@@ -1,20 +1,11 @@
-'use strict';
-
-var PARSE_VEGA = 'PARSE_VEGA',
-    INVALIDATE_VEGA = 'INVALIDATE_VEGA';
-
+import {createStandardAction} from 'typesafe-actions';
 /**
  * Action creator to set the state to represent an in-progress Lyra model parse.
  *
  * @param {boolean} value - Whether or not to mark a parse as being in-progress
  * @returns {Object} An action object
  */
-function parseVega(value) {
-  return {
-    type: PARSE_VEGA,
-    value: !!value
-  };
-}
+export const parseVega = createStandardAction('PARSE_VEGA')<boolean, null>();
 
 /**
  * Action creator to set a flag which will be used to reparse (destroy and
@@ -24,19 +15,4 @@ function parseVega(value) {
  * next store update cycle.
  * @returns {Object} An action object
  */
-function invalidateVega(shouldReparse) {
-  return {
-    type: INVALIDATE_VEGA,
-    value: !!shouldReparse
-  };
-}
-
-module.exports = {
-  // Action Names
-  PARSE_VEGA: PARSE_VEGA,
-  INVALIDATE_VEGA: INVALIDATE_VEGA,
-
-  // Action Creators
-  parseVega: parseVega,
-  invalidateVega: invalidateVega
-};
+export const invalidateVega = createStandardAction('INVALIDATE_VEGA')<boolean, null>();
