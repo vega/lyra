11619aadd6bdc9cb4434cc07be57fc4acd16b461
Jonathan Zong <jonathan.zong@gmail.com>
2019-02-14 14:12:15 -0500

    refactor most of signalsreducer

--- src/js/actions/signalActions.ts
+++ src/js/actions/signalActions.ts
@@ -1,12 +1,5 @@
 import {createStandardAction} from 'typesafe-actions';
-import {OnEvent} from 'vega-typings/types';
-
-const ns = require('../util/ns');
-
-    INIT_SIGNAL = 'INIT_SIGNAL',
-    SET_SIGNAL  = 'SET_SIGNAL',
-    SET_SIGNAL_STREAMS = 'SET_SIGNAL_STREAMS',
-    UNSET_SIGNAL = 'UNSET_SIGNAL';
+import {OnEvent, SignalValue} from 'vega-typings/types';
 
 /**
  * Action creator to initialize a signal.
@@ -15,7 +8,7 @@ const ns = require('../util/ns');
  * @param {*} value - The initial value of the signal
  * @returns {Object} An action object
  */
-export const initSignal = createStandardAction('INIT_SIGNAL')<any, string>();
+export const initSignal = createStandardAction('INIT_SIGNAL')<SignalValue, string>();
 
 /**
  * Action creator to configure the initial value of a signal.
@@ -24,7 +17,7 @@ export const initSignal = createStandardAction('INIT_SIGNAL')<any, string>();
  * @param {*} value - The value to set as the signal's initial value
  * @returns {Object} An action object
  */
-export const setSignal = createStandardAction('SET_SIGNAL')<any, string>();
+export const setSignal = createStandardAction('SET_SIGNAL')<SignalValue, string>();
 
 /**
  * Action creator to configure a property to update based on a stream.
--- src/js/reducers/signalsReducer.ts
+++ src/js/reducers/signalsReducer.ts
@@ -1,28 +1,29 @@
-/* eslint new-cap:0 */
-'use strict';
+import {Map} from 'immutable';
+import {ActionType, getType} from 'typesafe-actions';
+import {SignalValue} from 'vega-typings/types';
+import * as markActions from '../actions/markActions';
+import * as signalActions from '../actions/signalActions';
+import {Signal, SignalState} from '../ctrl/signals/defaults';
+import ns from '../util/ns';
 
-var Immutable = require('immutable'),
-    ns = require('../util/ns'),
-    ACTIONS = require('../actions/Names'),
-    propSg = require('../util/prop-signal'),
-    setIn = require('../util/immutable-utils').setIn;
+const propSg = require('../util/prop-signal');
 
 // Initialize a signal to a specific value
-function signalInit(state, action) {
-  return state.set(action.signal, Immutable.Map({
-    name: action.signal,
-    init: action.value,
+function signalInit(state: SignalState, signal: string, value: SignalValue): SignalState {
+  return state.set(signal, Signal({
+    name: ns(signal),
+    value: value,
     _idx: state.size
   }));
 }
 
 // Set the streams on a signal
-function setStreams(state, action) {
-  return setIn(state, action.signal + '.streams', action.value);
+function setStreams(state: SignalState, signal: string, value: SignalValue): SignalState {
+  return state.setIn([signal, 'on'], value);
 }
 
 // Initialize signals for any of the mark's properties that are defined with a .value
-function initSignalsForMark(state, action, props, propName) {
+function initSignalsForMark(state: SignalState, action: ActionType<typeof markActions.addMark>, props, propName?): SignalState {
   if (!props) {
     // No property values to initialize as signals; return state as-is
     return state;
@@ -30,25 +31,19 @@ function initSignalsForMark(state, action, props, propName) {
 
   /* eslint no-shadow:0 */
   // Initialize a signal to hold any specified update property values
-  return Object.keys(props).reduce(function(state, key) {
+  return Object.keys(props).reduce(function(accState, key) {
     if (typeof props[key].value === 'undefined') {
-      return state;
+      return accState;
     }
 
-    var type = action.type === ACTIONS.ADD_GUIDE ? 'guide' : action.props.type,
+    const type = action.type === ACTIONS.ADD_GUIDE ? 'guide' : action.props.type,
         name = action.type === ACTIONS.ADD_GUIDE ? propName + '_' + key : key,
         signalName = propSg(action.id, type, name),
         streams = action.streams;
 
-    var intermediateState = signalInit(state, {
-      signal: signalName,
-      value: props[key].value
-    });
+    const intermediateState = signalInit(accState, signalName, props[key].value);
 
-    return streams && streams[signalName] ? setStreams(intermediateState, {
-      signal: signalName,
-      value: streams[signalName]
-    }) : intermediateState;
+    return streams && streams[signalName] ? setStreams(intermediateState, signalName, streams[signalName]) : intermediateState;
   }, state);
 }
 
@@ -57,7 +52,7 @@ function deleteSignalsForMark(state, action) {
   // Create a regular expression which will match any signal that was created
   // for this mark (this works because signal names take a predictable form,
   // using the prop-signal module)
-  var markType = action.markType || 'guide',
+  const markType = action.markType || 'guide',
       markSignalRegex = new RegExp('^' + ns(markType + '_' + action.id));
   return state.filter(function(value, key) {
     return !markSignalRegex.test(key);
@@ -65,34 +60,34 @@ function deleteSignalsForMark(state, action) {
 }
 
 // @TODO: members of the state.signals map are not actually immutable
-function signalsReducer(state, action) {
+function signalsReducer(state: SignalState, action: ActionType<typeof signalActions | typeof markActions.addMark | typeof markActions.baseDeleteMark>): SignalState {
   if (typeof state === 'undefined') {
-    return Immutable.Map();
+    return Map();
   }
 
-  if (action.type === ACTIONS.INIT_SIGNAL) {
-    return signalInit(state, action);
+  if (action.type === getType(signalActions.initSignal)) {
+    return signalInit(state, action.meta, action.payload);
   }
 
-  if (action.type === ACTIONS.SET_SIGNAL) {
-    return setIn(state, action.signal + '.init', action.value);
+  if (action.type === getType(signalActions.setSignal)) {
+    return state.setIn([action.meta, 'value'], action.payload);
   }
 
-  if (action.type === ACTIONS.SET_SIGNAL_STREAMS) {
-    return setStreams(state, action);
+  if (action.type === getType(signalActions.setSignalStreams)) {
+    return setStreams(state, action.meta, action.payload);
   }
 
-  if (action.type === ACTIONS.UNSET_SIGNAL) {
-    return state.delete(action.signal);
+  if (action.type === getType(signalActions.unsetSignal)) {
+    return state.remove(action.meta);
   }
 
-  if (action.type === ACTIONS.ADD_MARK) {
+  if (action.type === getType(markActions.addMark)) {
     return initSignalsForMark(state, action,
-      action.props.properties && action.props.properties.update);
+      action.payload.props.encode && action.payload.props.encode.update);
   }
 
   if (action.type === ACTIONS.ADD_GUIDE) {
-    var props = action.props.properties;
+    const props = action.props.properties;
     Object.keys(props).forEach(function(key) {
       state = initSignalsForMark(state, action, props[key], key);
     });
