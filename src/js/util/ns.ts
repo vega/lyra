c7aad1637790c8610ebda0e20cdc3a7118aaceb4
Jonathan Zong <jonathan.zong@gmail.com>
2019-02-14 13:47:36 -0500

    type signals

--- src/js/util/ns.ts
+++ src/js/util/ns.ts
@@ -2,13 +2,13 @@
 // Ensure startsWith polyfill has loaded, to maintain IE compatibility
 require('string.prototype.startswith');
 
-var NS = 'lyra_';
+const NS = 'lyra_';
 
 /**
  * Ensure a string is properly namespaced with a lyra string prefix
  * @param  {string} name - A property name that may or may not be namespaced
  * @returns {string} A property name that is guaranteed to be namespaced
  */
-module.exports = function(name) {
+export default module.exports = function(name: string): string {
   return name.startsWith(NS) ? name : NS + name;
 };
