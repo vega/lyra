da6b67b0cac72a1f0bc6223a4d3771934873fef6
Jonathan Zong <jonathan.zong@gmail.com>
2019-02-19 18:14:39 -0500

    refactor walkthrough actions, reducer, store

--- src/js/walkthrough/index.ts
+++ src/js/walkthrough/index.ts
@@ -1,9 +1,33 @@
 /**
  * Instantiate walkthrough Json files here
  */
-'use strict';
+import {List} from 'immutable';
+import {VisStateTree} from '../store';
 
-module.exports = {
+export interface WalkthroughJSON {
+  title: string;
+  image: string;
+  alt_text: string;
+  steps: List<WalkthroughStepJSON>;
+
+}
+
+export interface WalkthroughStepJSON {
+  id: number;
+  title: string;
+  text: string;
+  image: string;
+  alt_text: string;
+  lyra_state: VisStateTree,
+  help: string;
+}
+
+export interface WalkthroughData {
+  example: WalkthroughJSON;
+  barChart: WalkthroughJSON;
+}
+
+export const data: WalkthroughData = {
   example: require('./files/example.json'),
   barChart: require('./files/example-different.json')
 };
