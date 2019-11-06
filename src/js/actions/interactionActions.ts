75792494ec9b00d6add4d5c23520e571b185ba38
Jonathan Zong <jonathan.zong@gmail.com>
2019-11-06 12:35:17 -0500

    panzoom

--- src/js/actions/interactionActions.ts
+++ src/js/actions/interactionActions.ts
@@ -6,6 +6,9 @@ const counter  = require('../util/counter');
 export const addInteraction = createStandardAction('ADD_INTERACTION').map((record: InteractionRecord) => {
   const id: number = record.id || counter.global();
   record = (record as any).merge({id: id}) as InteractionRecord;
+  if (!record.get('name')) {
+    record = record.set('name', "Interaction "+id);
+  }
 
   return {
     payload: record, meta: id
