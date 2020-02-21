cdfcfa32b7c8a7c45cf1281916bbcedc70d63d2d
Jonathan Zong <jonathan.zong@gmail.com>
2020-02-21 13:15:06 -0500

    improved interaction typings

--- src/js/store/factory/Interaction.ts
+++ src/js/store/factory/Interaction.ts
@@ -1,5 +1,6 @@
 import {Map, Record, RecordOf} from 'immutable';
-import {LyraSelectionPreviewDef, LyraApplicationPreviewDef} from '../../components/interactions/InteractionPreviewController';
+import InteractionPreview from '../../components/interactions/InteractionPreview';
+import {Signal} from 'vega';
 
 export interface PropertyValues {
   size: number,
@@ -10,10 +11,54 @@ export interface LyraInteraction {
   id: number;
   name: string;
   groupId: number;
-  selectionDef: LyraSelectionPreviewDef;
-  applicationDef: LyraApplicationPreviewDef;
+  selectionDef: LyraSelectionDef;
+  applicationDef: LyraApplicationDef;
   markPropertyValues: PropertyValues;
 };
+
+interface BaseLyraInteractionDef {
+  id: string;
+  label: string;
+  ref?: React.RefObject<InteractionPreview>;
+}
+
+interface BaseLyraSelectionDef {
+  signals: Signal[];
+}
+
+export type LyraPointSelectionDef = {
+  type: 'point';
+
+} & BaseLyraSelectionDef & BaseLyraInteractionDef;
+
+export type LyraIntervalSelectionDef = {
+  type: 'interval';
+} & BaseLyraSelectionDef & BaseLyraInteractionDef;
+
+export type LyraSelectionDef = LyraPointSelectionDef | LyraIntervalSelectionDef;
+
+interface BaseLyraApplicationDef {
+  groupName: string; // which group does this application affect? (for filters, different from the group it's attached to)
+}
+
+export type LyraMarkApplicationDef = {
+  type: 'mark';
+  markName: string; // which mark does this application affect?
+  markProperties: any; // partial mark spec object
+} & BaseLyraApplicationDef & BaseLyraInteractionDef;
+
+export type LyraScaleApplicationDef = {
+  type: 'scale';
+  scaleProperties: any[]; // list of partial scale objects
+} & BaseLyraApplicationDef & BaseLyraInteractionDef;
+
+export type LyraTransformApplicationDef = {
+  type: 'transform';
+  markProperties: any; // partial mark object,
+  datasetProperties: any; // partial dataset object
+} & BaseLyraApplicationDef & BaseLyraInteractionDef;
+
+export type LyraApplicationDef = LyraMarkApplicationDef | LyraScaleApplicationDef | LyraTransformApplicationDef;
 
 export const Interaction = Record<LyraInteraction>({
   id: null,
