62ce50fa9cd990f38ffae7a3dd957aaa2e10a3d8
Jonathan Zong <jonathan.zong@gmail.com>
2020-03-12 23:41:21 -0400

    pipelines!!

--- src/js/components/pipelines/PipelineList.tsx
+++ src/js/components/pipelines/PipelineList.tsx
@@ -4,32 +4,63 @@ import {connect} from 'react-redux';
 import {State} from '../../store';
 import {PipelineRecord} from '../../store/factory/Pipeline';
 import {PipelineInspector} from './PipelineInspector';
+import {selectPipeline} from '../../actions/inspectorActions';
+import {updatePipelineProperty as updatePipeline} from '../../actions/pipelineActions';
 
 const getInVis = require('../../util/immutable-utils').getInVis;
+const ContentEditable = require('../ContentEditable');
 
 interface OwnProps {
   openModal: () => void;
 }
 interface StateProps {
   pipelines: Map<number, PipelineRecord>;
+  selectedId: number;
+}
+
+interface DispatchProps {
+  selectPipeline: (id: number) => void;
+  updatePipeline: (payload: {property: string, value: any}, id: number) => void;
 }
 
 function mapState(state: State): StateProps {
   return {
-    pipelines: getInVis(state, 'pipelines')
+    pipelines: getInVis(state, 'pipelines'),
+    selectedId: state.getIn(['inspector', 'pipelines', 'selectedId'])
   };
 }
 
-class BasePipelineList extends React.Component<OwnProps & StateProps> {
+const mapDispatch: DispatchProps = {
+  selectPipeline,
+  updatePipeline
+};
+
+class BasePipelineList extends React.Component<OwnProps & StateProps & DispatchProps> {
   public render() {
-    const pipelines = this.props.pipelines.keySeq().toArray();
+    const pipelineIds = this.props.pipelines.keySeq().toArray();
+    const pipelines = this.props.pipelines.valueSeq().toArray();
 
     return (
       <div id='pipeline-list'>
+        <div className='pipeline-tabs'>
+          {
+            pipelines.length ?
+              pipelines.map((pipeline) => {
+                const id = pipeline._id;
+                const name = pipeline.name;
+                return (
+                  <ContentEditable key={id} className={'header ' + (this.props.selectedId == id ? 'selected' : '')}
+                  value={name}
+                  save={this.props.updatePipeline.bind(this, id, 'name')}
+                  onClick={() => this.props.selectPipeline(id)} />
+                );
+              }) : null
+          }
+        </div>
         {
-          pipelines.length ?
-            pipelines.map(function(id) {
-              return (<PipelineInspector key={id} id={id} />);
+          pipelineIds.length ?
+            pipelineIds.map((id) => {
+              return (<PipelineInspector key={id} id={id} selectedId={this.props.selectedId} />);
             }) :
             <div id='pipeline-hint' onClick={this.props.openModal}>
               Add a new data pipeline to get started.
@@ -40,4 +71,4 @@ class BasePipelineList extends React.Component<OwnProps & StateProps> {
   }
 }
 
-export const PipelineList = connect(mapState)(BasePipelineList);
+export const PipelineList = connect(mapState, mapDispatch)(BasePipelineList);
